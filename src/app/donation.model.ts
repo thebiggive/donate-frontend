7589ce6ff710d6267761b51d58250639fcfe8244
Noel Light-Hilary <noel@noellh.com>
2019-07-11 16:08:34 +0100

    DON-3 - update Donation properties
    Add some additional properties we need and make some existing
    ones optional

--- src/app/donation.model.ts
+++ src/app/donation.model.ts
@@ -1,32 +1,20 @@
 import { DonationStatus } from './donation-status.type';
 
+/**
+ * Many properties on `Donation` are nullable, because they are set only:
+ * * after the donation has been persisted in Salesforce (e.g. `status`, `createdTime`, ...); or
+ * * after the donor has completed the Charity Checkout forms (e.g. `countryCode`, `emailAddress`, ...); or
+ * * after the donation is fully processed and webhook returned (e.g. `matchedAmount`).
+ */
 export class Donation {
   constructor(
-    /**
-     * Donor's address including postcode.
-     */
-    public billingPostalAddress: string,
-
     /**
      * Unique ID for a charity Account assigned by the Big Give, in Salesforce
      * case-insensitive format. 18 character string.
      */
     public charityId: string,
 
-    /**
-     * Donor's country code in ISO 3166-1 alpha-2 format.
-     */
-    public countryCode: string,
-
-    public createdTime: Date,
-
     public donationAmount: number,
-
-    /**
-     * Unique ID for a donation, in Salesforce case-insensitive format. 18 character string.
-     * Assigned earlier than Charity Checkout's `transactionId`.
-     */
-    public donationId: string,
 
     /**
      * Indicates whether donation was expected to be eligible for either full or partial matching
@@ -35,23 +23,9 @@ export class Donation {
      */
     public donationMatched: boolean,
 
-    public emailAddress: string,
-
-    public firstName: string,
-
     public giftAid: boolean,
 
-    public lastName: string,
-
-    /**
-     * Amount actually matched once donation is Collected or Paid.
-     */
-    public matchedAmount: number,
-
-    /**
-     * Amount allocated for matching when donation initiated.
-     */
-    public matchReservedAmount: number,
+    public optInCharityEmail: boolean,
 
     public optInTbgEmail: boolean,
 
@@ -62,16 +36,52 @@ export class Donation {
     public projectId: string,
 
     /**
+     * Donor's address including postcode.
+     */
+    public billingPostalAddress?: string,
+
+    public charityName?: string,
+
+    /**
+     * Donor's country code in ISO 3166-1 alpha-2 format.
+     */
+    public countryCode?: string,
+
+    public createdTime?: Date,
+
+    /**
+     * Unique ID for a donation, in Salesforce case-insensitive format. 18 character string.
+     * Assigned earlier than Charity Checkout's `transactionId`.
+     */
+    public donationId?: string,
+
+    public emailAddress?: string,
+
+    public firstName?: string,
+
+    public lastName?: string,
+
+    /**
+     * Amount actually matched once donation is Collected or Paid.
+     */
+    public matchedAmount?: number,
+
+    /**
+     * Amount allocated for matching when donation initiated.
+     */
+    public matchReservedAmount?: number,
+
+    /**
      * One of the Charity Checkout status strings defined in the `DonationStatus` type.
      * See `donation-status.enum.ts` where the type and backing enum are defined.
      */
-    public status: DonationStatus,
+    public status?: DonationStatus,
 
     /**
      * ID assigned by Charity Checkout upon checkout initiation.
      */
-    public transactionId: string,
+    public transactionId?: string,
 
-    public updatedTime: Date,
-  ) {}
+    public updatedTime?: Date,
+    ) {}
 }
