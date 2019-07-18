4ebcff71e2f80503aee85ff10ac3d206b36df533
Noel Light-Hilary <noel@noellh.com>
2019-07-18 16:44:07 +0100

    Update <h1> title assertions

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display heading', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('The Big Give Donations');
+    expect(page.getTitleText()).toContain('Donations');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('The Big Give Donations');
+    expect(compiled.querySelector('h1').textContent).toContain('Donations');
   });
 });
