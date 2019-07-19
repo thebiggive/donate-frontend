ff0e21f857ef00b117abd89e11f99f8056fed74b
tbg-jasonfung <jason@thebiggive.org.uk>
2019-07-19 17:19:09 +0100

    Fix h1 expect value

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('2.1');
+    expect(compiled.querySelector('h1').textContent).toContain('Donations');
   });
 });
