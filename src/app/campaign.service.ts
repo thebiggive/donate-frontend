3c5b1459b9308c4beb5eaf24ac0ceffdfd327559
tbg-jasonfung <jason@thebiggive.org.uk>
2019-07-23 15:57:53 +0100

    Remove trailing white space

--- src/app/campaign.service.ts
+++ src/app/campaign.service.ts
@@ -17,10 +17,8 @@ export class CampaignService {
   constructor(
     private http: HttpClient,
   ) {}
-  
   /* tslint:disable:max-line-length*/
   getCampaign(campaignId): Observable<Campaign[]> {
-    
     return this.http.get<Campaign[]>(`${environment.apiUriPrefix}${this.apiPath}` + campaignId);
   }
   /* tslint:enable:max-line-length*/
