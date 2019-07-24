c11d0f7043ea7120c2098661cb942f7d18f1b908
tbg-jasonfung <jason@thebiggive.org.uk>
2019-07-24 12:51:28 +0100

    Remove unused tags, tidy up spacing, adjust date datatype

--- src/app/campaign.service.spec.ts
+++ src/app/campaign.service.spec.ts
@@ -32,7 +32,7 @@ describe('CampaignService', () => {
           person: 'Someones quote',
         },
       ],
-      startDate: '2019-07-03T15:30:00.000Z',
+      startDate: new Date(),
       isMatched: true,
       id: 'a051r00001EywjpAAB',
       giftHandles: [
@@ -47,7 +47,7 @@ describe('CampaignService', () => {
           id: '0011r00002HHAprAAH',
         },
       ],
-      endDate: '2019-07-10T13:30:00.000Z',
+      endDate: new Date(),
       championName: 'The Big Give Match Fund',
       budgetDetails: [
         {
@@ -55,15 +55,13 @@ describe('CampaignService', () => {
           amount: 2000.01,
         },
       ],
-      /* tslint:disable:max-line-length*/
+      /* tslint:disable-next-line */
       bannerUri: 'https://thebiggive--Full--c.cs101.content.force.com/sfc/dist/version/download/?oid=00D1X0000008ahG&ids=0681X0000007w5l&d=%2Fa%2F1X0000008UpU%2FvdeP6lXwciVq6CMSA8BhwNQ6hNTyZHu7VmcugZgN2SY&asPdf=false',
-      /* tslint:enable:max-line-length*/
       amountRaised: 200.00,
       additionalImageUris: [
         {
-          /* tslint:disable:max-line-length*/
+          /* tslint:disable-next-line */
           uri: 'https://thebiggive--c.eu12.content.force.com/sfc/dist/version/download/?oid=00D0O000000YzQm&ids=0681r00000CtcvY&d=%2Fa%2F1r0000002Ypn%2FeDMuuQUiOagDxe4259w1_.zlHnaPgj2nMiGJ5M.Gi2Y&asPdf=false',
-          /* tslint:enable:max-line-length*/
           order: 100,
         },
       ],
