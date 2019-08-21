41e61ad48ea0cb6d1e7355e4f15e580f2b781118
Noel Light-Hilary <noel@noellh.com>
2019-08-21 17:48:11 +0100

    Fix production API prefix

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUriPrefix: 'https://biggive.force.com',
+  apiUriPrefix: 'https://biggive.secure.force.com',
   charityCheckoutInitUri: 'https://fundraise.charitycheckout.co.uk/api/checkout/init',
   donateUriPrefix: 'https://donate.thebiggive.org.uk',
   maximumDonationAmount: 25000,
