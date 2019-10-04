d43ad962a6d0f355228563c85685f5c3b6680990
Noel Light-Hilary <noel@noellh.com>
2019-10-04 05:57:47 +0100

    DON-42 - fix error dialog `data` type

--- src/app/donation-start/donation-start-error-dialog.component.ts
+++ src/app/donation-start/donation-start-error-dialog.component.ts
@@ -1,12 +1,10 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-import { Donation } from '../donation.model';
-
 @Component({
   selector: 'app-donation-start-error-dialog',
   templateUrl: 'donation-start-error-dialog.html',
 })
 export class DonationStartErrorDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { donation: Donation }) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { charityCheckoutError: string }) { }
 }
