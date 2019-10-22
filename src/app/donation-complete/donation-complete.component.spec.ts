d81de63b2f92dfdfeb331226dbb57fac7d7e9a33
Noel Light-Hilary <noel@noellh.com>
2019-10-22 13:53:04 +0100

    DON-72 - hack-y fix for global var GA test issue

--- src/app/donation-complete/donation-complete.component.spec.ts
+++ src/app/donation-complete/donation-complete.component.spec.ts
@@ -7,6 +7,11 @@ import { of } from 'rxjs';
 
 import { DonationCompleteComponent } from './donation-complete.component';
 
+// Hack to ensure calls to the global from AnalyticsService as the component loads don't cause test
+// errors. TODO - ideally, we should have a mock of the AnalyticsService for tests which doesn't touch
+// the global and allows us to assert that specific GA calls are made.
+const gtag = () => {};
+
 describe('DonationCompleteComponent', () => {
   let component: DonationCompleteComponent;
   let fixture: ComponentFixture<DonationCompleteComponent>;
@@ -37,6 +42,8 @@ describe('DonationCompleteComponent', () => {
   }));
 
   beforeEach(() => {
+
+
     fixture = TestBed.createComponent(DonationCompleteComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
