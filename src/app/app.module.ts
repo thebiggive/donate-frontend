8d412aaf799ed8b45f086e4582128abfd8bc0656
Noel Light-Hilary <noel@noellh.com>
2019-11-06 19:06:17 +0000

    DON-84 - carousel SSR workaround
    Fix carousel crashing server-rendered `CampaignDetailsComponent`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,16 +19,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CampaignCardComponent } from './campaign-card/campaign-card.component';
+import { CampaignDetailsComponent } from './campaign-details/campaign-details.component';
 import { CampaignSearchFormComponent } from './campaign-search-form/campaign-search-form.component';
+import { DonationCompleteComponent } from './donation-complete/donation-complete.component';
 import { DonationStartComponent } from './donation-start/donation-start.component';
 import { DonationStartErrorDialogComponent } from './donation-start/donation-start-error-dialog.component';
 import { DonationStartMatchConfirmDialogComponent } from './donation-start/donation-start-match-confirm-dialog.component';
 import { DonationStartOfferReuseDialogComponent } from './donation-start/donation-start-offer-reuse-dialog.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
-import { CampaignDetailsComponent } from './campaign-details/campaign-details.component';
 import { MetaCampaignComponent } from './meta-campaign/meta-campaign.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 import { TimeLeftPipe } from './time-left.pipe';
-import { DonationCompleteComponent } from './donation-complete/donation-complete.component';
 
 @NgModule({
   declarations: [
@@ -36,6 +36,7 @@ import { DonationCompleteComponent } from './donation-complete/donation-complete
     CampaignCardComponent,
     CampaignDetailsComponent,
     CampaignSearchFormComponent,
+    DonationCompleteComponent,
     DonationStartComponent,
     DonationStartErrorDialogComponent,
     DonationStartMatchConfirmDialogComponent,
@@ -43,7 +44,6 @@ import { DonationCompleteComponent } from './donation-complete/donation-complete
     SearchResultsComponent,
     MetaCampaignComponent,
     TimeLeftPipe,
-    DonationCompleteComponent,
   ],
   entryComponents: [
     DonationStartErrorDialogComponent,
@@ -57,6 +57,7 @@ import { DonationCompleteComponent } from './donation-complete/donation-complete
     HttpClientModule,
     MatButtonModule,
     MatCardModule,
+    MatCarouselModule.forRoot(),
     MatDialogModule,
     MatGridListModule,
     MatInputModule,
@@ -65,7 +66,6 @@ import { DonationCompleteComponent } from './donation-complete/donation-complete
     MatSelectModule,
     MatProgressSpinnerModule,
     ReactiveFormsModule,
-    MatCarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
