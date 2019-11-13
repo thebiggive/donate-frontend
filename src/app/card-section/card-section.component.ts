2961ac5da2a135733d34ecec403eda604a4b6c80
James Horne <james@daughter.is>
2019-11-13 17:35:03 +0000

    Card section ts initial commit

---
+++ src/app/card-section/card-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-section',
+  templateUrl: './card-section.component.html',
+  styleUrls: ['./card-section.component.scss']
+})
+export class CardSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
