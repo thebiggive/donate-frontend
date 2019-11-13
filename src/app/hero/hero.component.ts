1f3ca24729e15f3350eceb643a05fb33fd3c7ae5
James Horne <james@daughter.is>
2019-11-13 14:29:33 +0000

    Add hero ts

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface Category {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss'],
+})
+export class HeroComponent implements OnInit {
+
+  constructor() { }
+
+  category: Category[] = [
+    {value: 'category-1', viewValue: 'Category 1'},
+    {value: 'category-2', viewValue: 'Category 2'},
+    {value: 'category-3', viewValue: 'Category 3'},
+  ];
+
+  ngOnInit() {
+  }
+
+}
