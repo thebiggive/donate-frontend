b79a2dd6d0495e58230c85f67e1f71636a2c1684
Noel Light-Hilary <noel@noellh.com>
2019-11-14 12:08:12 +0000

    Expand CampaignSummary data

--- src/app/campaign-summary.model.ts
+++ src/app/campaign-summary.model.ts
@@ -4,11 +4,13 @@ export class CampaignSummary {
   constructor(
     public id: string,
     public amountRaised: number,
+    public categories: string[],
     public championName: string,
     public charity: {id: string, name: string},
     public endDate: Date,
     public imageUri: string,
     public isMatched: boolean,
+    public matchFundsRemaining: number,
     public startDate: Date,
     public target: number,
     public title: string,
