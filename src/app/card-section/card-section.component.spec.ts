ed3c6c652aa87798e2a9d4c94e2119fb21f748b7
James Horne <james@daughter.is>
2019-11-14 17:55:58 +0000

    DON-49 Remove icon module

--- src/app/card-section/card-section.component.spec.ts
+++ src/app/card-section/card-section.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {
-  MatInputModule,
   MatSelectModule,
 } from '@angular/material';
 
@@ -18,7 +17,6 @@ describe('CardSectionComponent', () => {
         FiltersComponent,
       ],
       imports: [
-        MatInputModule,
         MatSelectModule,
       ],
     })
