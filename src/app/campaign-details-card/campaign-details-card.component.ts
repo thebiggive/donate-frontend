3bf2ef9c03017f2958365a0d27fa252c0724c4e5
Noel Light-Hilary <noel@noellh.com>
2019-11-18 10:56:13 +0000

    Lint fix

--- src/app/campaign-details-card/campaign-details-card.component.ts
+++ src/app/campaign-details-card/campaign-details-card.component.ts
@@ -5,12 +5,12 @@ import { Campaign } from '../campaign.model';
 @Component({
   selector: 'app-campaign-details-card',
   templateUrl: './campaign-details-card.component.html',
-  styleUrls: ['./campaign-details-card.component.scss']
+  styleUrls: ['./campaign-details-card.component.scss'],
 })
 export class CampaignDetailsCardComponent implements OnInit {
 
   @Input() public campaign: Campaign;
-  
+
   constructor() { }
 
   ngOnInit() {
