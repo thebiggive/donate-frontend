5d99b4f7f2ebb17221e264d73797b8967b391d10
Noel Light-Hilary <noel@noellh.com>
2019-11-27 22:45:28 +0000

    Switch Production to MatchBot

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUriPrefix: 'https://sf-api-production.thebiggive.org.uk',
   charityCheckoutInitUri: 'https://fundraise.charitycheckout.co.uk/api/checkout/init',
   donateUriPrefix: 'https://donate.thebiggive.org.uk',
-  donationsApiPrefix: 'https://biggive.secure.force.com/donations/services/apexrest/v1.0',
+  donationsApiPrefix: 'https://matchbot-production.thebiggive.org.uk/v1',
   googleAnalyticsId: 'UA-2979952-1',
   maximumDonationAmount: 25000,
   thanksUriPrefix: 'https://donate.thebiggive.org.uk/thanks/',
