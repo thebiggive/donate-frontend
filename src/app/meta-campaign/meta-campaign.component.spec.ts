6a937bc1ba273ca157b15fa2d9a7000667505cfe
Noel Light-Hilary <noel@noellh.com>
2019-11-28 12:50:09 +0000

    DON-154 - update test imports

--- src/app/meta-campaign/meta-campaign.component.spec.ts
+++ src/app/meta-campaign/meta-campaign.component.spec.ts
@@ -14,6 +14,7 @@ import {
 import { BrowserTransferStateModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { Campaign } from '../campaign.model';
 import { CampaignCardComponent } from '../campaign-card/campaign-card.component';
@@ -43,6 +44,7 @@ describe('MetaCampaignComponent', () => {
       imports: [
         BrowserTransferStateModule,
         HttpClientTestingModule,
+        InfiniteScrollModule,
         MatButtonModule, // Not required but makes test DOM layout more realistic
         MatCardModule,
         MatGridListModule,
