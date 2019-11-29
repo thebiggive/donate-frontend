712289fd229349aeffaca2696bcc9ff1de2fea57
Noel Light-Hilary <noel@noellh.com>
2019-11-29 12:01:08 +0000

    Revert "DON-155 - fix tests for async Pipe + add more"
    This reverts commit def84e97

--- src/app/time-left.pipe.spec.ts
+++ src/app/time-left.pipe.spec.ts
@@ -1,64 +1,32 @@
-import { ChangeDetectorRef } from '@angular/core';
-
 import { TimeLeftPipe } from './time-left.pipe';
 
 describe('TimeLeftPipe', () => {
-  let pipe: TimeLeftPipe;
-
-  beforeEach(() => {
-    const cdRefMock: ChangeDetectorRef = {
-      checkNoChanges: () => null,
-      detach: () => null,
-      detectChanges: () => null,
-      markForCheck: () => null,
-      reattach: () => null,
-    };
-    pipe = new TimeLeftPipe(cdRefMock);
-  });
-
   it('is instantiated', () => {
+    const pipe = new TimeLeftPipe();
     expect(pipe).toBeTruthy();
   });
 
-  it('returns 0 seconds for past times', () => {
-    const futureDate = new Date((new Date()).getTime() - 100000);
-
-    expect(pipe.transform(futureDate)).toEqual('0 secs');
-  });
-
-  it('returns 0 seconds for very near future times', () => {
-    const futureDate = new Date((new Date()).getTime() + 100); // 0.1 seconds in the future
-
-    expect(pipe.transform(futureDate)).toEqual('0 secs');
-  });
-
-  it('returns 1 sec correctly', () => {
-    const futureDate = new Date((new Date()).getTime() + 1100); // 1.1 seconds in the future
-
-    expect(pipe.transform(futureDate)).toEqual('1 sec');
-  });
-
-  it('returns 1 min correctly', () => {
+  it('returns 1 minute correctly', () => {
     const futureDate = new Date((new Date()).getTime() + 61000); // 61 seconds in the future
 
-    expect(pipe.transform(futureDate)).toEqual('1 min');
+    expect((new TimeLeftPipe()).transform(futureDate)).toEqual('1 minute');
   });
 
   it('returns 23 hours correctly', () => {
     const futureDate = new Date((new Date()).getTime() + 23 * 60 * 60 * 1000 + 10000); // 23 hours and 10 seconds in the future
 
-    expect(pipe.transform(futureDate)).toEqual('23 hours');
+    expect((new TimeLeftPipe()).transform(futureDate)).toEqual('23 hours');
   });
 
   it('returns 1 day correctly', () => {
     const futureDate = new Date((new Date()).getTime() + 86410000); // 1 day and 10 seconds in the future
 
-    expect(pipe.transform(futureDate)).toEqual('1 day');
+    expect((new TimeLeftPipe()).transform(futureDate)).toEqual('1 day');
   });
 
   it('returns 1,000 days correctly', () => {
     const futureDate = new Date((new Date()).getTime() + 1000 * 86410000); // 1,000 days and 10 seconds in the future
 
-    expect(pipe.transform(futureDate)).toEqual('1,000 days');
+    expect((new TimeLeftPipe()).transform(futureDate)).toEqual('1,000 days');
   });
 });
