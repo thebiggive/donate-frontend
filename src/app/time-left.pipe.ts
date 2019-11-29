1919e2df67357dac0d0a2b983164a3d6f3146791
Noel Light-Hilary <noel@noellh.com>
2019-11-29 13:36:55 +0000

    DON-155 - SSR-safe async timeLeft pipe
    This should work as intended before on the client
    side, while generating a single value during server
    rendering and having the SSR Observable resolved
    immediately.

--- src/app/time-left.pipe.ts
+++ src/app/time-left.pipe.ts
@@ -1,14 +1,19 @@
-import { DecimalPipe } from '@angular/common';
-import { Pipe, PipeTransform } from '@angular/core';
+import { AsyncPipe, DecimalPipe, isPlatformBrowser } from '@angular/common';
+import { ChangeDetectorRef, Inject, Pipe, PipeTransform, PLATFORM_ID } from '@angular/core';
+import { interval, Observable, of } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 /**
  * Pipe to summarise the time remaining to a given date.
+ * Realtime update / AsyncPipe adaptation based on https://stackoverflow.com/a/36667685/2803757
  */
 @Pipe({
   name: 'timeLeft',
+  pure: false,
 })
-export class TimeLeftPipe implements PipeTransform {
-  private unitDefinitions = [
+export class TimeLeftPipe extends AsyncPipe implements PipeTransform {
+    // static for access from object context and in interval callbacks
+    public static unitDefinitions = [
     {
       label: 'day',
       value: 86400000,
@@ -18,7 +23,7 @@ export class TimeLeftPipe implements PipeTransform {
       value: 3600000,
     },
     {
-      label: 'min',
+      label: 'min', // We abbreviate to 'mins' and 'secs' to keep current card layout looking good
       value: 60000,
     },
     {
@@ -27,13 +32,20 @@ export class TimeLeftPipe implements PipeTransform {
     },
   ];
 
-  transform(date: Date | string): string {
-    if (!(date instanceof Date)) { // Support ISO 8601 strings as returned by the Salesforce API
-      date = new Date(date);
-    }
+  timer: Observable<string>;
+  value: Date;
 
-    for (const unit of this.unitDefinitions) {
-      const wholeUnits = this.buildForUnit(date, unit.value);
+  constructor(
+    // tslint:disable-next-line:ban-types Angular types this ID as `Object` so we must follow suit.
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private ref: ChangeDetectorRef,
+  ) {
+    super(ref);
+  }
+
+  public static format(date: Date) {
+    for (const unit of TimeLeftPipe.unitDefinitions) {
+      const wholeUnits = TimeLeftPipe.buildForUnit(date, unit.value);
       if (wholeUnits >= 1) {
         return (new DecimalPipe('en-GB')).transform(wholeUnits, '1.0-0') + ` ${unit.label}` + (wholeUnits > 1 ? 's' : '');
       }
@@ -42,7 +54,34 @@ export class TimeLeftPipe implements PipeTransform {
     return '0 secs';
   }
 
-  private buildForUnit(date: Date, microsecsInUnit: number): number {
+  public static buildForUnit(date: Date, microsecsInUnit: number): number {
     return Math.floor((date.getTime() - (new Date()).getTime()) / microsecsInUnit);
+  }
+
+  transform(date: any, args?: any[]): any {
+    if (!(date instanceof Date)) { // Support ISO 8601 strings as returned by the Salesforce API
+      date = new Date(date);
+    }
+
+    if (!date) {
+      return super.transform(date);
+    }
+
+    this.value = date;
+    if (!this.timer) {
+      this.timer = this.getObservable();
+    }
+
+    return super.transform(this.timer);
+  }
+
+  private getObservable(): Observable<string> {
+    // On the browser / client-side JS, we set a 1 second interval so times can 'tick' live.
+    if (isPlatformBrowser(this.platformId)) {
+      return interval(1000).pipe(startWith(0), map(() => TimeLeftPipe.format(this.value)));
+    }
+
+    // On the server, get the value once; if we set an interval here, SSR spins forever and breaks the app!
+    return of(TimeLeftPipe.format(this.value));
   }
 }
