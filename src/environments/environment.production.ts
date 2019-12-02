80c4143f2f8988c94aded2e7a74acf265a177fda
Noel Light-Hilary <noel@noellh.com>
2019-12-02 15:01:23 +0000

    Prod config change: disable all suggested amounts

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -6,13 +6,8 @@ export const environment = {
   donationsApiPrefix: 'https://matchbot-production.thebiggive.org.uk/v1',
   googleAnalyticsId: 'UA-2979952-1',
   maximumDonationAmount: 25000,
-  // Each of 4x suggestion sets for 5% of donors each, no suggestions for the other 80%.
   suggestedAmounts: [
-    { weight: 1,  values: [30, 100, 250] },
-    { weight: 1,  values: [50, 200, 500] },
-    { weight: 1,  values: [20,  50, 100] },
-    { weight: 1,  values: [20, 100, 500] },
-    { weight: 16, values: [] },
+    { weight: 1, values: [] },
   ],
   thanksUriPrefix: 'https://donate.thebiggive.org.uk/thanks/',
 };
