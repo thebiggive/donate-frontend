9d325d3ec1922e8888990aeb61fe7e65729f30a8
Noel Light-Hilary <noel@noellh.com>
2020-02-25 12:22:15 +0000

    @nguniversal/express-engine migration - update-9 Update @nguniversal/express-engine to version 9.

--- server.ts
+++ server.ts
@@ -1,88 +1,60 @@
 import 'zone.js/dist/zone-node';
-import 'reflect-metadata';
-import { enableProdMode } from '@angular/core';
+
 import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
-import * as compression from 'compression';
 import * as express from 'express';
-import { Request, Response } from 'express';
-import * as helmet from 'helmet';
-import * as morgan from 'morgan';
 import { join } from 'path';
 
-import { environment } from './src/environments/environment';
+import { AppServerModule } from './src/main.server';
+import { APP_BASE_HREF } from '@angular/common';
+import { existsSync } from 'fs';
 
-// Faster server renders w/ Prod mode (dev mode never needed)
-enableProdMode();
+// The Express app is exported so that it can be used by serverless Functions.
+export function app() {
+  const server = express();
+  const distFolder = join(process.cwd(), 'dist/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
-// Express server
-const app = express();
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+  server.engine('html', ngExpressEngine({
+    bootstrap: AppServerModule,
+  }));
 
-app.use(compression());
-app.use(helmet()); // Sane header defaults, e.g. remove powered by, add HSTS, stop MIME sniffing etc.
-app.use(morgan('combined')); // Log requests to stdout in Apache-like format
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
 
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist/browser');
+  // Example Express Rest API endpoints
+  // app.get('/api/**', (req, res) => { });
+  // Serve static files from /browser
+  server.get('*.*', express.static(distFolder, {
+    maxAge: '1y'
+  }));
 
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
-
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-// @ts-ignore Suppress param type error for now. See https://github.com/angular/universal/issues/1210
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP),
-  ],
-}));
-
-app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
-
-app.get('/robots.txt', (req: Request, res: Response) => {
-  res.type('text/plain');
-  if (environment.production) {
-    res.send('User-agent: *\nAllow: /');
-  } else {
-    res.send('User-agent: *\nDisallow: /');
-  }
-});
-
-// Serve static files requested via /d/ from /browser/d - when deployed S3 serves these up to CloudFront
-app.get('/d/favicon.ico', (req: Request, res: Response) => {
-  res.sendFile(DIST_FOLDER + '/favicon.ico', {
-    maxAge: '7 days', // Don't make the favicon immutable in case we want to update it
+  // All regular routes use the Universal engine
+  server.get('*', (req, res) => {
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
   });
-});
 
-app.use('/d', express.static(DIST_FOLDER, {
-  immutable: true, // Everything in here should be named with an immutable hash
-  maxAge: '1 year',
-}));
+  return server;
+}
 
-app.use('/assets', express.static(DIST_FOLDER + '/assets', {
-  maxAge: '1 day', // Assets should be served similarly but don't have name-hashes
-}));
+function run() {
+  const port = process.env.PORT || 4000;
 
-// All regular routes use the Universal engine
-app.get('*', (req: Request, res: Response) => {
-  // Note that the file output as `index.html` is actually dynamic. See `index` config keys in `angular.json`.
-  // See https://github.com/angular/angular-cli/issues/10881#issuecomment-530864193 for info on the undocumented use of
-  // this key to work around `fileReplacements` ending index support in Angular 8.
-  res.render('index', { req });
-});
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
 
-// Start up the Node server
-const server = app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
-});
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = mainModule && mainModule.filename || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
 
-/**
- * ALBs are configured with 60s timeout and these should be longer.
- * @link https://shuheikagawa.com/blog/2019/04/25/keep-alive-timeout/
- * @link https://adamcrowder.net/posts/node-express-api-and-aws-alb-502/
- */
-server.keepAliveTimeout = 65 * 1000;
-server.timeout = 70 * 1000;
+export * from './src/main.server';
