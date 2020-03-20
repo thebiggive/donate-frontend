fa4220e3edd7128056fab96241669432dca8576b
Noel Light-Hilary <noel@noellh.com>
2020-03-20 22:25:17 +0000

    DON-202 - manage Navigation state properly
    Use an actual navigation event, consistently, to control
    menu open + close events that are navigation-linked,
    removing all `(click)` events from the NavigationComponent
    template except the actual state toggle button.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router, RouterEvent } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
@@ -8,8 +10,11 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationComponent implements OnInit {
   opened = false;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.router.events.pipe(
+      filter((event: RouterEvent) => event instanceof NavigationStart),
+    ).subscribe(() => this.opened = false);
   }
 }
