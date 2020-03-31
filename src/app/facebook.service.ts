f5a87c1b71acd18cb0a959de08a4b698453daffc
Noel Light-Hilary <noel@noellh.com>
2020-03-31 13:29:21 +0100

    DON-204 - fix Facebook pixel init + PageView

--- src/app/facebook.service.ts
+++ src/app/facebook.service.ts
@@ -1,49 +1,56 @@
 import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 import { environment } from '../environments/environment';
+
+declare const fbq: (action: string, eventOrId: string, params?: object) => void;
 
 @Injectable({
   providedIn: 'root',
 })
 export class FacebookService {
-  constructor() {}
+  constructor(private router: Router) {}
 
   init() {
-    this.facebookJS();
+    if (!environment.facebookPixelId) {
+      return;
+    }
+
+    this.listenForRouteChanges();
+
+    const scriptInitFacebook = document.createElement('script');
+    scriptInitFacebook.innerHTML = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window,document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+
+      fbq('init', '` + environment.facebookPixelId + `');
+    `;
+    document.head.appendChild(scriptInitFacebook);
   }
 
-  /**
-   * Inspired by https://stackoverflow.com/a/58120781/2803757
-   */
-  private facebookJS() {
-    ((f: any, b?: any, e?: any, v?: any, n?: any, t?: any, s?: any) => {
-      if (f.fbq) {
-        return;
+  trackConversion(amount: number) {
+    if (!fbq) {
+      return; // Skip the call gracefully if loading fails or 3rd party JS is blocked.
+    }
+
+    fbq('track', 'Donate', {value: amount, currency: 'GBP'});
+  }
+
+  private listenForRouteChanges() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (!fbq) {
+          return; // Skip the call gracefully if loading fails or 3rd party JS is blocked.
+        }
+
+        fbq('track', 'PageView');
       }
-      n = f.fbq = () => {
-        n.callMethod
-          ? n.callMethod.apply(n, arguments)
-          : n.queue.push(arguments);
-      };
-      if (!f._fbq) {
-        f._fbq = n;
-      }
-      n.push = n;
-      n.loaded = !0;
-      n.version = '2.0';
-      n.queue = [];
-      t = b.createElement(e);
-      t.async = !0;
-      t.src = v;
-      s = b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t, s);
-    })(
-      window,
-      document,
-      'script',
-      'https://connect.facebook.net/en_US/fbevents.js',
-    );
-    (window as any).fbq('init', environment.facebookPixelId);
-    (window as any).fbq('track', 'PageView');
+    });
   }
 }
