89c98d5e94fd7a25b34072037f41f22c7aed8c6f
Noel Light-Hilary <noel@noellh.com>
2020-06-06 16:16:37 +0100

    Drop icon loading flash workaround attempt
    On further testing this still seemed not to
    help with the SSR handover in Firefox, so
    I now suspect may not be helping while
    adding app weight. In Chrome, the blip is not
    occurring even in Production which doesn't yet have the
    attempted fix.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, ElementRef, Inject, OnInit, PLATFORM_ID, Renderer2 } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Router } from '@angular/router';
-import * as FontFaceObserver from 'fontfaceobserver';
 
 import { AnalyticsService } from './analytics.service';
 import { GetSiteControlService } from './getsitecontrol.service';
@@ -14,27 +13,16 @@ import { GetSiteControlService } from './getsitecontrol.service';
 export class AppComponent implements OnInit {
   constructor(
     private analyticsService: AnalyticsService,
-    private elementRef: ElementRef,
     private getSiteControlService: GetSiteControlService,
     // tslint:disable-next-line:ban-types Angular types this ID as `Object` so we must follow suit.
     @Inject(PLATFORM_ID) private platformId: Object,
-    private renderer: Renderer2,
     private router: Router,
   ) {}
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
-      // Avoid flash of icon placeholder text during SSR -> client JS handover.
-      // https://github.com/angular/components/issues/12171#issuecomment-546079738
-      const materialIcons = new FontFaceObserver('Material Icons');
-      materialIcons.load(null, 10000)
-        .then(() => this.renderer.addClass(this.elementRef.nativeElement, 'material-icons-loaded'));
-
       this.analyticsService.init();
       this.getSiteControlService.init();
-    } else {
-      // On the server side, add the class to show icons immediately.
-      this.renderer.addClass(this.elementRef.nativeElement, 'material-icons-loaded');
     }
   }
 
