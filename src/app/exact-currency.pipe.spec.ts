1849f6d3118a0287bbcd7a356ef42ba49a299b3e
Noel Light-Hilary <noel@noellh.com>
2020-06-19 12:16:27 +0100

    DON-218 - better decimal handling for exact currency values

---
+++ src/app/exact-currency.pipe.spec.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectorRef, PLATFORM_ID } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
+
+import { ExactCurrencyPipe } from './exact-currency.pipe';
+
+describe('ExactCurrencyPipe', () => {
+  let pipe: ExactCurrencyPipe;
+
+  beforeEach(async(() => {
+    TestBed.compileComponents();
+  }));
+
+  beforeEach(() => {
+    const cdRefMock: ChangeDetectorRef = {
+      checkNoChanges: () => null,
+      detach: () => null,
+      detectChanges: () => null,
+      markForCheck: () => null,
+      reattach: () => null,
+    };
+    pipe = new ExactCurrencyPipe();
+  });
+
+  it('is instantiated', () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  it('shows no decimal with whole numbers of pounds', () => {
+    expect(pipe.transform(12345)).toEqual('£12,345');
+  });
+
+  it('pads correctly with £*.50', () => {
+    expect(pipe.transform(1234.5)).toEqual('£1,234.50');
+  });
+
+  it('works with values not requiring padding', () => {
+    expect(pipe.transform(123.45)).toEqual('£123.45');
+  });
+
+  it('rounds unexpected > 2 d.p. values to 2 d.p.', () => {
+    expect(pipe.transform(12.345)).toEqual('£12.35');
+  });
+});
