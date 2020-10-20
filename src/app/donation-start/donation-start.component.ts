c3bff5848b114fe00e3f7b453c7159bd3ef6c25c
Noel Light-Hilary <noel@noellh.com>
2020-10-20 20:02:59 +0200

    DON-267 - use cross-platform-safe Timer type

--- src/app/donation-start/donation-start.component.ts
+++ src/app/donation-start/donation-start.component.ts
@@ -72,7 +72,7 @@ export class DonationStartComponent implements AfterContentChecked, OnDestroy, O
   // but modified to make the separating space optional.
   private postcodeRegExp = new RegExp('^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\\s?[0-9][A-Za-z]{2})$');
   /** setTimeout reference (timer ID) if applicable. */
-  expiryWarning?: number;
+  expiryWarning?: ReturnType<typeof setTimeout>; // https://stackoverflow.com/a/56239226
 
   constructor(
     private analyticsService: AnalyticsService,
