818744af6903c1dbba4dba9d680c9206bd586dbc
tbg-jasonfung <jason@thebiggive.org.uk>
2020-11-02 10:38:11 +0000

    Reduce page weight and hardcode youtube embed url in template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 import { CampaignService, SearchQuery } from '../campaign.service';
 import { CampaignSummary } from '../campaign-summary.model';
@@ -13,23 +12,19 @@ import { CampaignSummary } from '../campaign-summary.model';
 export class HomeComponent implements OnInit {
   public campaigns: CampaignSummary[];
   public loading = false; // Server render gets initial result set; set true when filters change.
-  public resetSubject: Subject<void> = new Subject<void>();
-  public videoEmbedUrl: SafeResourceUrl;
-  public youtubeId = '5RWDzzBg-go';
+  public resetSubject: Subject<void> = new Subject<void>();;
 
   private perPage = 6;
   private query: {[key: string]: any};
 
   constructor(
     private campaignService: CampaignService,
-    private sanitizer: DomSanitizer,
   ) {
   }
 
   ngOnInit() {
     this.setDefaults();
     this.run();
-    this.videoEmbedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.youtubeId}`);
   }
 
   setDefaults() {
