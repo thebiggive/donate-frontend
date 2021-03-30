24f46e38f6f3aba79b3cd78f41d5d642ea851f3a
Noel Light-Hilary <noel@noellh.com>
2021-03-30 20:29:09 +0100

    DON-373 - fix quotes

--- server.ts
+++ server.ts
@@ -57,8 +57,8 @@ export function app() {
           'https:',
         ],
         'script-src': [
-          'strict-dynamic',
-          'unsafe-inline', // Backwards compat. https://csp-evaluator.withgoogle.com/
+          `'strict-dynamic'`,
+          `'unsafe-inline'`, // Backwards compat. https://csp-evaluator.withgoogle.com/
           donateHost,
           `'sha256-lAAe/2BNa8LfOLFsGspOHNtIPGU+RpI2Ne1/HaNdnLE='`, // IE fallback inline script?
           `'sha256-${createHash('sha256').update(AnalyticsService.getConfigureContent()).digest('base64')}'`,
