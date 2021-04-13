e7fc09de189aa063bf5792b72a79058b56bc1c5e
Noel Light-Hilary <noel@noellh.com>
2021-04-13 17:56:54 +0100

    DON-233 - support Payment Request Buttons
    First pass at showing and responding to
    PRB use, including Google Analytics logging of
    eCommerce success and errors encountered

--- src/app/stripe.service.spec.ts
+++ src/app/stripe.service.spec.ts
@@ -1,12 +1,26 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { InMemoryStorageService } from 'ngx-webstorage-service';
 
+import { TBG_DONATE_STORAGE } from './donation.service';
 import { StripeService } from './stripe.service';
 
 describe('StripeService', () => {
   let service: StripeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        InMemoryStorageService,
+        // Inject in-memory storage for tests, in place of local storage.
+        { provide: TBG_DONATE_STORAGE, useExisting: InMemoryStorageService },
+      ]
+    });
     service = TestBed.inject(StripeService);
   });
 
