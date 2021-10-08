8e547ccb3c168229983e037859ff2ebc54b77d68
alihejazi <35169573+alihejazi@users.noreply.github.com>
2021-10-08 15:21:32 +0100

    Revert "DON-420 - turn off psps.stripe.prbEnabled in Staging, for testing"

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -21,7 +21,7 @@ export const environment: Environment = {
     },
     stripe: {
       enabled: true,
-      prbEnabled: false, // Temporary QA test
+      prbEnabled: true, // Payment Request Buttons – Apple & Google Pay
       publishableKey: 'pk_test_51GxbdTKkGuKkxwBNorvoPNKbbvEAwCjxfxOBd8lFZWAVkbJoXdFEDXOrbBbebAotP0vqLSntrLzs0Fvr7P7n0yjO00E3c61L5W',
     },
   },
