d83444675d3c0f21dd12ee89c6de0e5941b044f7
Noel Light-Hilary <noel@noellh.com>
2022-07-26 21:58:45 +0100

    COM-9 – experimental possible Angular output target config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,14 @@
+import { angularOutputTarget } from '@stencil/angular-output-target';
 import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'BigGive',
   outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: '@biggive/components',
+      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
