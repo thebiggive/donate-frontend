ff0805a31592bb3c4642a6e3efff267698595c14
Noel Light-Hilary <noel@noellh.com>
2022-07-28 17:42:02 +0100

    COM-9 – document a couple of open config mysteries

--- angular/projects/components/src/public-api.ts
+++ angular/projects/components/src/public-api.ts
@@ -2,4 +2,7 @@
  * Public API Surface of components
  */
 
+// This bails out with 'Unsupported private class'. TODO why?
+// export { ComponentsModule } from './lib/components.module';
+
 export { DIRECTIVES } from './lib/stencil-generated';
