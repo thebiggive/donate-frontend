3cca68b8510f9aac5c49734a5f5c436091d10749
Noel Light-Hilary <noel@noellh.com>
2022-07-28 17:40:53 +0100

    COM-5, COM-9 – import CommonModule
    Ionic does this and it seems like it might help with the
    unknown pipes etc. crash we've seen in the Angular app
    test.

--- angular/projects/components/src/lib/components.module.ts
+++ angular/projects/components/src/lib/components.module.ts
@@ -1,7 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DIRECTIVES } from './stencil-generated';
 
 @NgModule({
+  imports: [CommonModule],
   declarations: [...DIRECTIVES],
   exports: [...DIRECTIVES],
 })
