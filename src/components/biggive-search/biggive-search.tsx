64bbec248c0da5ff5a8cc1a82c7ef4242e11d126
alihejazi <ahejazi1999@gmail.com>
2022-09-09 16:55:55 +0100

    COM-15 added tests

--- src/components/biggive-search/biggive-search.tsx
+++ src/components/biggive-search/biggive-search.tsx
@@ -1,5 +1,4 @@
 import { Component, h } from '@stencil/core';
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -14,17 +13,16 @@ export class BigGiveSearch {
         <h2>Find a Charity</h2>
 
         <div class="search-box">
-          <svg width="512" height="512" xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512">
             <path d={faMagnifyingGlass.icon[4].toString()} />
           </svg>
 
           <input type="text" placeholder="Search" />
 
-          <svg width="512" height="512" xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512">
             <path d={faX.icon[4].toString()} />
           </svg>
         </div>
-
 
         <button>Search</button>
       </div>
