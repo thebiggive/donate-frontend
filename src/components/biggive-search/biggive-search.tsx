a9f9e2942bc9cb349872cfd57fc8348e769845e2
alihejazi <ahejazi1999@gmail.com>
2022-09-09 16:17:43 +0100

    COM-15 Added search and X icons

--- src/components/biggive-search/biggive-search.tsx
+++ src/components/biggive-search/biggive-search.tsx
@@ -1,16 +1,31 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, h } from '@stencil/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   tag: 'biggive-search',
   styleUrl: 'biggive-search.scss',
-  shadow: false,
+  shadow: true,
 })
 export class BigGiveSearch {
   render() {
     return (
       <div class="container">
         <h2>Find a Charity</h2>
-        <input type="text" placeholder='Search'/>
+
+        <div class="search-box">
+          <svg width="512" height="512" xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512">
+            <path d={faMagnifyingGlass.icon[4].toString()} />
+          </svg>
+
+          <input type="text" placeholder="Search" />
+
+          <svg width="512" height="512" xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512">
+            <path d={faX.icon[4].toString()} />
+          </svg>
+        </div>
+
+
         <button>Search</button>
       </div>
     );
