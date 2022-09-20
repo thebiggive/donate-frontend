9d4c776fd38e64dc6f13c42f47aa991f02a91e9b
alihejazi <ahejazi1999@gmail.com>
2022-09-20 10:48:46 +0100

    COM-15 used newer design for component

--- src/components/biggive-search/biggive-search.tsx
+++ src/components/biggive-search/biggive-search.tsx
@@ -40,14 +40,14 @@ export class BigGiveSearch {
   render() {
     return (
       <div class="container">
-        <h2>Find a Charity</h2>
+        <h2>Find a charity or project</h2>
 
         <div class="search-box">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon fa-thin" viewBox="0 0 512 512">
             <path d={faMagnifyingGlass.icon[4].toString()} />
           </svg>
 
-          <input type="text" placeholder={this.placeholderText} onInput={event => this.handleSearchTextChanged(event)} onKeyDown={event => this.handleEnterPressed(event)} />
+          <input type="text" value={this.searchText} placeholder={this.placeholderText} onInput={event => this.handleSearchTextChanged(event)} onKeyDown={event => this.handleEnterPressed(event)} />
 
           <svg xmlns="http://www.w3.org/2000/svg" class="icon" id="x-icon" viewBox="0 0 512 512" onClick={() => this.clearSearchText()}>
             <path d={faX.icon[4].toString()} />
