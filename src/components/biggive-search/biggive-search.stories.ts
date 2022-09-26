8d5364cdea7fd79d5be10657a34f34aa44f71cac
alihejazi <ahejazi1999@gmail.com>
2022-09-26 14:40:57 +0100

    COM-15 linting fixes

--- src/components/biggive-search/biggive-search.stories.ts
+++ src/components/biggive-search/biggive-search.stories.ts
@@ -2,16 +2,15 @@ export default {
   title: 'Components/Demos',
 };
 
-const Template = (args) => `<biggive-search placeholderText="${args.placeholderText}" buttonText="${args.buttonText}" doSearch="${args.doSearch}"></biggive-search>`;
+const Template = args => `<biggive-search placeholderText="${args.placeholderText}" buttonText="${args.buttonText}" doSearch="${args.doSearch}"></biggive-search>`;
 
 const search = () => {
   alert('Search Button Pressed');
 };
 
-
 export const SearchComponent = Template.bind({});
 SearchComponent.args = {
   placeholderText: 'Search',
   buttonText: 'Search',
-  doSearch: search
+  doSearch: search,
 };
