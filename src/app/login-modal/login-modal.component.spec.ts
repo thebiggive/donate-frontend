c44df49d634309f51d5c10f7c51e48bc9019e65f
alihejazi <ahejazi1999@gmail.com>
2022-10-26 16:28:19 +0100

    DON-532 fixing build issues #3

---
+++ src/app/login-modal/login-modal.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+import { LoginModalComponent } from './login-modal.component';
+
+describe('LoginModalComponent', () => {
+  let component: LoginModalComponent;
+  let fixture: ComponentFixture<LoginModalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginModalComponent ],
+      imports: [
+        MatDialog,
+        MatDialogRef,
+        MatDialogModule
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
