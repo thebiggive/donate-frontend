03a8447e11c2f5197d3c52d91b78717f6ef162bd
Noel Light-Hilary <noel@noellh.com>
2022-10-27 13:35:58 +0100

    DON-532 – uncomment + fix new component tests

--- src/app/buy-credits/buy-credits.component.spec.ts
+++ src/app/buy-credits/buy-credits.component.spec.ts
@@ -1,88 +1,81 @@
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-// import { FlexLayoutModule } from '@angular/flex-layout';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatCheckboxModule } from '@angular/material/checkbox';
-// import { MatDialogModule } from '@angular/material/dialog';
-// import { MatIconModule } from '@angular/material/icon';
-// import { MatInputModule } from '@angular/material/input';
-// import { MatProgressBarModule } from '@angular/material/progress-bar';
-// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-// import { MatRadioModule } from '@angular/material/radio';
-// import { MatSelectModule } from '@angular/material/select';
-// import { MatStepperModule } from '@angular/material/stepper';
-// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { RecaptchaModule } from 'ng-recaptcha';
-// import { InMemoryStorageService } from 'ngx-webstorage-service';
-// import { TBG_DONATE_STORAGE } from '../donation.service';
-// import { TBG_DONATE_ID_STORAGE } from '../identity.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RecaptchaModule } from 'ng-recaptcha';
+import { InMemoryStorageService } from 'ngx-webstorage-service';
+import { TBG_DONATE_STORAGE } from '../donation.service';
+import { TBG_DONATE_ID_STORAGE } from '../identity.service';
 
-// import { BuyCreditsComponent } from './buy-credits.component';
+import { BuyCreditsComponent } from './buy-credits.component';
 
-// describe('BuyCreditsComponent', () => {
-//   let component: BuyCreditsComponent;
-//   let fixture: ComponentFixture<BuyCreditsComponent>;
+describe('BuyCreditsComponent', () => {
+  let component: BuyCreditsComponent;
+  let fixture: ComponentFixture<BuyCreditsComponent>;
 
-//   // beforeEach(async () => {
-//   //   await TestBed.configureTestingModule({
-//   //     declarations: [ BuyCreditsComponent ],
-//   //     imports: [
-//   //       FormsModule,
-//   //       MatDialogModule,
-//   //       ReactiveFormsModule,
-//   //     ]
-//   //   })
-//   //   .compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        BuyCreditsComponent,
+      ],
+      imports: [
+        FlexLayoutModule,
+        FormsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatDialogModule,
+        MatIconModule,
+        MatInputModule,
+        MatRadioModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatSelectModule,
+        MatStepperModule,
+        NoopAnimationsModule,
+        ReactiveFormsModule,
+        RecaptchaModule,
+        RouterTestingModule.withRoutes([
+          {
+            path: 'buy-credits',
+            component: BuyCreditsComponent,
+          },
+        ]),
+      ],
+      providers: [
+        InMemoryStorageService,
+        { provide: TBG_DONATE_ID_STORAGE, useExisting: InMemoryStorageService },
+        { provide: TBG_DONATE_STORAGE, useExisting: InMemoryStorageService },
+      ],
+    });
 
-//   //   fixture = TestBed.createComponent(BuyCreditsComponent);
-//   //   component = fixture.componentInstance;
-//   //   fixture.detectChanges();
-//   // });
+    TestBed.compileComponents();
+  }));
 
-//   beforeEach(waitForAsync(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [
-//         BuyCreditsComponent,
-//       ],
-//       imports: [
-//         HttpClientTestingModule,
-//         MatButtonModule,
-//         MatCheckboxModule,
-//         MatDialogModule,
-//         FlexLayoutModule,
-//         MatIconModule,
-//         MatInputModule,
-//         MatRadioModule,
-//         MatProgressBarModule,
-//         MatProgressSpinnerModule,
-//         MatSelectModule,
-//         MatStepperModule,
-//         NoopAnimationsModule,
-//         ReactiveFormsModule,
-//         RecaptchaModule,
-//         RouterTestingModule.withRoutes([
-//           {
-//             path: 'buy-credits',
-//             component: BuyCreditsComponent,
-//           },
-//         ]),
-//       ],
-//       providers: [
-//         InMemoryStorageService,
-//         { provide: TBG_DONATE_ID_STORAGE, useExisting: InMemoryStorageService },
-//         { provide: TBG_DONATE_STORAGE, useExisting: InMemoryStorageService },
-//       ],
-//     }).compileComponents();
-//   }));
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+    }).compileComponents();
 
-//   // it('should create', () => {
-//   //   expect(component).toBeTruthy();
-//   // });
+    fixture = TestBed.createComponent(BuyCreditsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     fixture.detectChanges();
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
