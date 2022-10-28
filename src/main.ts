1c691403a7aca228865bd723916c8b51f023c8b0
Noel Light-Hilary <noel@noellh.com>
2022-10-28 09:06:32 +0100

    DON-524 – lazy load routes + drop routing module

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,16 @@ import { RECAPTCHA_NONCE } from 'ng-recaptcha';
 import { LOCAL_STORAGE } from 'ngx-webstorage-service';
 
 import { AppComponent } from './app/app.component';
+import { CampaignListResolver } from './app/campaign-list.resolver';
+import { CampaignPromoted1Resolver } from './app/campaign-promoted-1.resolver';
+import { CampaignPromoted2Resolver } from './app/campaign-promoted-2.resolver';
+import { CampaignResolver } from './app/campaign.resolver';
+import { CharityCampaignsResolver } from './app/charity-campaigns.resolver';
+import { environment } from './environments/environment';
 import { routes } from './app/app-routing';
 import { TBG_DONATE_STORAGE } from './app/donation.service';
 import { TBG_DONATE_ID_STORAGE } from './app/identity.service';
-import { environment } from './environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 if (environment.productionLike) {
   enableProdMode();
@@ -20,7 +26,19 @@ if (environment.productionLike) {
 globalThis.document.addEventListener('DOMContentLoaded', () => {
   bootstrapApplication(AppComponent, {
     providers: [
-      importProvidersFrom(RouterModule.forRoot(routes)),
+      // Vendor dependencies for resolvers etc.
+      HttpClientModule,
+
+      CampaignResolver,
+      CampaignListResolver,
+      CampaignPromoted1Resolver,
+      CampaignPromoted2Resolver,
+      CharityCampaignsResolver,
+      importProvidersFrom(RouterModule.forRoot(routes, {
+        initialNavigation: 'enabledBlocking', // "This value is required for server-side rendering to work." https://angular.io/api/router/InitialNavigation
+        onSameUrlNavigation: 'reload', // Allows Explore & home logo links to clear search filters in ExploreComponent
+        scrollPositionRestoration: 'enabled',
+      })),
       // In Universal / SSR mode, `APP_BASE_HREF` should vary according to the host reported
       // by the browser once client side JS takes over. This is necessary so we can successfully
       // serve the app on multiple live domains.
