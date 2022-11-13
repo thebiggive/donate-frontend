89de5e23b39666c9c82c5e55777e3fe6082d0dca
Noel Light-Hilary <noel@noellh.com>
2022-11-13 20:56:41 +0000

    DON-635 – make navigation + footer non-standalone

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,14 +4,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { allChildComponentImports } from '../../allChildComponentImports';
 
 @Component({
-  standalone: true,
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  imports: [
-    ...allChildComponentImports,
-    MatButtonModule,
-  ],
 })
 export class FooterComponent {
   public now: number = Date.now();
