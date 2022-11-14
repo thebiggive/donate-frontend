f8385f6c2b4d0d9a46b614aec6c40a3c01b72217
Ali Hejazi <ahejazi1999@gmail.com>
2022-11-14 16:16:29 +0000

    DON-589 quotes now looking good

--- src/components/biggive-quote/biggive-quote.tsx
+++ src/components/biggive-quote/biggive-quote.tsx
@@ -1,4 +1,5 @@
 import { Component, Prop, h } from '@stencil/core';
+import { faQuoteLeft } from '@fortawesome/pro-solid-svg-icons';
 
 @Component({
   tag: 'biggive-quote',
@@ -24,11 +25,20 @@ export class BiggiveQuote {
    */
   @Prop() attribution: string = '';
 
+  icon = faQuoteLeft;
+  iconSize = 50;
+
   render() {
     return (
       <div class={'container text-colour-' + this.defaultTextColour + ' space-below-' + this.spaceBelow}>
-        <div class="quote">{this.quote}</div>
-        <div class="attribution">{this.attribution}</div>
+        <svg width={this.iconSize} height={this.iconSize} xmlns="http://www.w3.org/2000/svg" class="fill-primary" viewBox={'0 0 ' + this.icon.icon[0] + ' ' + this.icon.icon[1]}>
+          <path d={this.icon.icon[4].toString()} />
+        </svg>
+
+        <div class="quote">"{this.quote}"</div>
+        <br></br>
+        <br></br>
+        <div class="attribution"> - {this.attribution}</div>
       </div>
     );
   }
