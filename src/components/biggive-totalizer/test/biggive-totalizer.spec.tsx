87f0896a6a196d9e8ace4d06e505c664dc7e344c
Ali Hejazi <ahejazi1999@gmail.com>
2022-11-14 15:17:31 +0000

    DON-607 ticker can now take <biggive-totalizer-ticker-item/> children via slots; array prop not needed.

--- src/components/biggive-totalizer/test/biggive-totalizer.spec.tsx
+++ src/components/biggive-totalizer/test/biggive-totalizer.spec.tsx
@@ -15,7 +15,9 @@ describe('biggive-totalizer', () => {
                <div class="banner">
                  <div class="background-colour-secondary main-message-wrap text-colour-black"></div>
                  <div class="background-colour-primary text-colour-white ticker-wrap">
-                   <div class="sleeve" style="animation-duration: NaNs;"></div>
+                   <div class="sleeve" style="animation-duration: NaNs;">
+                     <slot name="ticker-items"></slot>
+                   </div>
                  </div>
                </div>
              </div>
