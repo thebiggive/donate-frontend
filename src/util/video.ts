cea35986ed13746afe8e264c87f77c67def96615
Noel Light-Hilary <noel@noellh.com>
2022-11-15 13:52:37 +0000

    DON-642 – lazy load heavy YouTube iframes

--- src/util/video.ts
+++ src/util/video.ts
@@ -1,7 +1,7 @@
 export class VideoService {
   static getEmbedHtml(url) {
     if (url.match(/youtube\.com/g)) {
-      return '<iframe src="' + url + '"></iframe>';
+      return '<iframe loading="lazy" src="' + url + '"></iframe>';
     } else {
       return '<video controls src=' + url + '></video>';
     }
