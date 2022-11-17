9bbb4ee7397381304fc34e7a2645a529d2dfae91
Noel Light-Hilary <noel@noellh.com>
2022-11-17 19:43:05 +0000

    Minor new brand social share + Stripe PRB info tweaks

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -50,8 +50,8 @@ export class ExploreComponent implements OnDestroy, OnInit {
 
   ngOnInit() {
     this.pageMeta.setCommon(
-      'The Big Give',
-      'The Big Give &ndash; discover campaigns and donate',
+      'Big Give',
+      'Big Give – discover campaigns and donate',
       false,
       'https://images-production.thebiggive.org.uk/0011r00002IMRknAAH/CCampaign%20Banner/db3faeb1-d20d-4747-bb80-1ae9286336a3.jpg',
     );
