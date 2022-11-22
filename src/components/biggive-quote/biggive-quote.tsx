7efe23c569f0df43ae941a04d7b713789223631e
tomwpaterson-shape <tom@shapehistory.com>
2022-11-22 16:52:49 +0000

    Quote lint fix

--- src/components/biggive-quote/biggive-quote.tsx
+++ src/components/biggive-quote/biggive-quote.tsx
@@ -28,9 +28,9 @@ export class BiggiveQuote {
     return (
       <div class={'container text-colour-' + this.defaultTextColour + ' space-below-' + this.spaceBelow}>
         <div class="image-wrap">
-        <svg width="34" height="24" viewBox="0 0 34 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16.1245 0L13.7167 24H0L11.0901 0H16.1245ZM34 0L31.5923 24H17.8755L28.9657 0H34Z" fill="#2C089B"/>
-        </svg>
+          <svg width="34" height="24" viewBox="0 0 34 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.1245 0L13.7167 24H0L11.0901 0H16.1245ZM34 0L31.5923 24H17.8755L28.9657 0H34Z" fill="#2C089B" />
+          </svg>
         </div>
         <div class="quote">“{this.quote}”</div>
         <div class="attribution"> - {this.attribution}</div>
