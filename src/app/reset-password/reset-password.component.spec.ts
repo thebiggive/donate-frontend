3a1dcbf01a75c8c00274604bfb0cce512085ae51
Ali Hejazi <ahejazi1999@gmail.com>
2022-12-06 17:23:11 +0000

    DON-272 test passing

--- src/app/reset-password/reset-password.component.spec.ts
+++ src/app/reset-password/reset-password.component.spec.ts
@@ -1,16 +1,74 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RecaptchaModule } from 'ng-recaptcha';
+import { InMemoryStorageService } from 'ngx-webstorage-service';
+import { TBG_DONATE_STORAGE } from '../donation.service';
+import { TBG_DONATE_ID_STORAGE } from '../identity.service';
 
 import { ResetPasswordComponent } from './reset-password.component';
+
 
 describe('ResetPasswordComponent', () => {
   let component: ResetPasswordComponent;
   let fixture: ComponentFixture<ResetPasswordComponent>;
 
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+      ],
+      imports: [
+        FlexLayoutModule,
+        FormsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatDialogModule,
+        MatIconModule,
+        MatInputModule,
+        MatRadioModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatSelectModule,
+        MatStepperModule,
+        NoopAnimationsModule,
+        ReactiveFormsModule,
+        RecaptchaModule,
+        RouterTestingModule.withRoutes([
+          {
+            path: 'reset-password',
+            component: ResetPasswordComponent,
+          },
+        ]),
+      ],
+      providers: [
+        InMemoryStorageService,
+        { provide: TBG_DONATE_ID_STORAGE, useExisting: InMemoryStorageService },
+        { provide: TBG_DONATE_STORAGE, useExisting: InMemoryStorageService },
+      ],
+    });
+
+    TestBed.compileComponents();
+  }));
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ResetPasswordComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ResetPasswordComponent);
     component = fixture.componentInstance;
