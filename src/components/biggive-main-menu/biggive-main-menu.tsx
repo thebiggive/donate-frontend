b6d21a85f289b68f4b82df3c0bb6d17974325a64
Ali Hejazi <ahejazi1999@gmail.com>
2022-12-23 11:30:56 +0000

    COM-28 externalised menu links so they're now slotted in

--- src/components/biggive-main-menu/biggive-main-menu.tsx
+++ src/components/biggive-main-menu/biggive-main-menu.tsx
@@ -16,6 +16,16 @@ export class BiggiveMainMenu {
     mobileMenu.style.left = '-100%';
   };
 
+  appendPrimaryNavigationLinks() {
+    // get the slotted 'nav-primary' node
+    const node = this.host.querySelector(`[slot="nav-primary"]`);
+
+    if (node !== null) {
+      // Slot the menu
+      this.host.shadowRoot.querySelector('#nav-primary').appendChild(node);
+    }
+  }
+
   /**
    * Slots the 'nav-secondary' slot into the top right corner of the blue nav bar.
    * Also slots the 'nav-secondary' into the normal menu items when in mobile mode.
@@ -45,7 +55,7 @@ export class BiggiveMainMenu {
 
   componentDidRender() {
     // Sidebar submenu open close js code for mobile menu
-    const subMenuArrows = this.host.shadowRoot.querySelectorAll('.sub-menu-arrow') as NodeListOf<HTMLElement>;
+    const subMenuArrows = this.host.querySelectorAll('.sub-menu-arrow') as NodeListOf<HTMLElement>;
     subMenuArrows.forEach(subMenuArrow => {
       subMenuArrow.onclick = () => {
         subMenuArrow.classList.toggle('transform-90');
@@ -56,7 +66,7 @@ export class BiggiveMainMenu {
     });
 
     // Sidebar sub-sub-menu open close js code for mobile menu
-    const subSubMenuArrows = this.host.shadowRoot.querySelectorAll('.sub-sub-menu-arrow') as NodeListOf<HTMLElement>;
+    const subSubMenuArrows = this.host.querySelectorAll('.sub-sub-menu-arrow') as NodeListOf<HTMLElement>;
     subSubMenuArrows.forEach(subSubMenuArrow => {
       subSubMenuArrow.onclick = () => {
         subSubMenuArrow.classList.toggle('transform-90');
@@ -66,6 +76,7 @@ export class BiggiveMainMenu {
       };
     });
 
+    this.appendPrimaryNavigationLinks();
     this.appendNavSecondaryLinks();
   }
 
@@ -97,106 +108,7 @@ export class BiggiveMainMenu {
                 </svg>
                 <biggive-misc-icon class="bx bx-x" background-colour="transparent" icon-colour="black" icon="MenuClose" onClick={this.closeMobileMenu}></biggive-misc-icon>
               </div>
-              <ul class="links">
-                <li>
-                  <a href="#">Explore Campaigns</a>
-                </li>
-                <li>
-                  <a href="#">Match Funding</a>
-                  <biggive-misc-icon class="bx bxs-chevron-down sub-menu-arrow arrow" background-colour="white" icon-colour="black" icon="CaretRight"></biggive-misc-icon>
-
-                  <ul class="sub-menu">
-                    <li>
-                      <a href="#">Match Funding Explained</a>
-                    </li>
-                    <li class="more">
-                      <span>
-                        <a href="#">Match Funding Opportunities</a>
-                        {/* IMPORTANT: notice this one has a class sub-sub-menu, not sub-menu */}
-                        <biggive-misc-icon class="bx bxs-chevron-down sub-sub-menu-arrow arrow" background-colour="white" icon-colour="black" icon="CaretRight"></biggive-misc-icon>
-                      </span>
-                      <ul class="sub-sub-menu">
-                        <li>
-                          <a href="#" class="icon-green">
-                            Christmas Challenge
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#" class="icon-children">
-                            Champions for Children
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#" class="icon-green-match">
-                            Green Match Fund
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#" class="icon-women-girls">
-                            Women & Girls Match Fund
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#" class="icon-emergency">
-                            Emergency Match Fund
-                          </a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      <a href="#">Run your match funding campaign</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="#">About us</a>
-                  <biggive-misc-icon class="bx bxs-chevron-down sub-menu-arrow arrow" background-colour="white" icon-colour="black" icon="CaretRight"></biggive-misc-icon>
-                  <ul class="sub-menu">
-                    <li>
-                      <a href="#">Our Story</a>
-                    </li>
-                    <li>
-                      <a href="#">Our People</a>
-                    </li>
-                    <li>
-                      <a href="#">Our Community</a>
-                    </li>
-                    <li>
-                      <a href="#">Our Fees</a>
-                    </li>
-                    <li>
-                      <a href="#">FAQs</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="#">For Charities</a>
-                </li>
-                <li>
-                  <a href="#">For Funders</a>
-                </li>
-                <li>
-                  <a href="#">Resources</a>
-                  <biggive-misc-icon class="bx bxs-chevron-down sub-menu-arrow arrow" background-colour="white" icon-colour="black" icon="CaretRight"></biggive-misc-icon>
-                  <ul class="sub-menu">
-                    <li>
-                      <a href="#">Case Studies</a>
-                    </li>
-                    <li>
-                      <a href="#">Webinars</a>
-                    </li>
-                    <li>
-                      <a href="#">Blog</a>
-                    </li>
-                    <li>
-                      <a href="#">Reports & Insights</a>
-                    </li>
-                    <li>
-                      <a href="#">Press</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
+              <div id="nav-primary"></div>
               <div class="mobile-social-icon-wrap mobile-only">
                 <slot name="mobile-social-icons"></slot>
               </div>
