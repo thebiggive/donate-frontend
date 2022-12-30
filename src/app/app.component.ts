2c7477b6c1d7e22edd163a519c862be2eb42b8aa
Noel Light-Hilary <noel@noellh.com>
2022-12-30 12:41:22 +0000

    Stop loading global styles both globally and on `AppComponent`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,6 @@ import { NavigationService } from './navigation.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit, OnInit {
   @ViewChild(BiggiveHeader) header: BiggiveHeader;
