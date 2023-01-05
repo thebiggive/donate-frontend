abd8e0d154a1f7af10f171fd050809fae0d41326
Barney Laurance <barney@thebiggive.org.uk>
2023-01-05 11:20:30 +0000

    DON-715: Add comment to link script

--- link-components.sh
+++ link-components.sh
@@ -3,6 +3,8 @@ set -x
 set -e
 cd "$(dirname "$0")"
 
+# git clean -fX forces deletion only of gitignored files, to ensure all compilation is re-run from scratch
+
 (cd ../components && git clean -fX angular dist loader hydrate && npm install && npm run build && npm link)
 (cd ../components/angular && npm install && npm run build && npm link)
 
