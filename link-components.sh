853875bb79ab3f1ced32c2e4c2d140571a43c598
Barney Laurance <barney@thebiggive.org.uk>
2023-01-05 14:37:19 +0000

    DON-715: Update package json in components/angular when linking

--- link-components.sh
+++ link-components.sh
@@ -6,6 +6,8 @@ cd "$(dirname "$0")"
 # git clean -fX forces deletion only of gitignored files, to ensure all compilation is re-run from scratch
 
 (cd ../components && git clean -fX angular dist loader hydrate && npm install && npm run build && npm link)
-(cd ../components/angular && npm install && npm run build && npm link)
+
+# Need to do npm update to make sure we don't have an outdated package-lock file refering to old version of components
+(cd ../components/angular && npm update && npm run build && npm link)
 
 npm link @biggive/components-angular @biggive/components
