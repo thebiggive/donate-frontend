3033b2f487d31b393393b458ee6c0ac91ce77cb5
alihejazi <35169573+alihejazi@users.noreply.github.com>
2023-01-13 17:46:36 +0000

    Use Math.min instead
    Co-authored-by: Barney Laurance <barney@thebiggive.org.uk>

--- src/components/biggive-progress-bar/biggive-progress-bar.tsx
+++ src/components/biggive-progress-bar/biggive-progress-bar.tsx
@@ -24,7 +24,7 @@ export class BiggiveProgressBar {
     return (
       <div class={'progress-bar progress-bar-' + this.colourScheme + ' space-below-' + this.spaceBelow}>
         <div class="slider">
-          <div class="progress" style={{ width: `${this.counter >= 100 ? 100 : this.counter}%` }}></div>
+          <div class="progress" style={{ width: `${Math.min(this.counter, 100)}%` }}></div>
         </div>
         <div class="counter">{this.counter}%</div>
       </div>
