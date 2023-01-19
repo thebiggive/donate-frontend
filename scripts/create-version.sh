3f4e1983aba7036d779fcc0b58d78794aea0a850
Barney Laurance <barney@thebiggive.org.uk>
2023-01-19 18:28:56 +0000

    COM-14: WIP automatic version publishing

--- scripts/create-version.sh
+++ scripts/create-version.sh
@@ -3,4 +3,16 @@
 # Bash strict mode // http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
+cd "$(dirname "$0")"/..
 
+set -x
+
+VERSION_NUMBER=`date +%Y.%m.%d.%H.%M.%S`
+
+echo $VERSION_NUMBER
+
+npm version $VERSION_NUMBER
+cd angular
+npm version $VERSION_NUMBER
+
+git diff
