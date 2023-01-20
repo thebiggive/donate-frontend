f97c21b9010f35e0e9d8a4fa4d732e14666dcfd1
Barney Laurance <barney@thebiggive.org.uk>
2023-01-20 10:57:15 +0000

    COM-14: Adjust automatic version no style to v202301201056.0.0 date based format

--- scripts/create-version.sh
+++ scripts/create-version.sh
@@ -7,7 +7,9 @@ cd "$(dirname "$0")"/..
 
 set -x
 
-VERSION_NUMBER=`date +%Y.%m.%d.%H.%M.%S`
+# Generate a version number such as v202301201056.0.0
+# Publishing will fail and have to be manually retried if this script is used twice in the same minute.
+VERSION_NUMBER=`date +%Y%m%d%H%M.0.0`
 
 echo $VERSION_NUMBER
 
