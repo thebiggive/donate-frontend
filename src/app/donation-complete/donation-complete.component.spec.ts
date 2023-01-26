d03d1dd05b0a5d561d4df30c1d4c6f2eea206add
Barney Laurance <barney@thebiggive.org.uk>
2023-01-26 12:04:12 +0000

    DON-572: Add remaining tests for VIP donation logic

--- src/app/donation-complete/donation-complete.component.spec.ts
+++ src/app/donation-complete/donation-complete.component.spec.ts
@@ -1,58 +1,18 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { MatButtonModule } from '@angular/material/button';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { ActivatedRoute } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
-import { InMemoryStorageService } from 'ngx-webstorage-service';
-import { of } from 'rxjs';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {ActivatedRoute} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {InMemoryStorageService} from 'ngx-webstorage-service';
+import {of} from 'rxjs';
 
-import { AnalyticsService } from '../analytics.service';
-import {DonationService, TBG_DONATE_STORAGE} from '../donation.service';
-import { DonationCompleteComponent } from './donation-complete.component';
-import {IdentityService, TBG_DONATE_ID_STORAGE} from '../identity.service';
+import {AnalyticsService} from '../analytics.service';
+import {TBG_DONATE_STORAGE} from '../donation.service';
+import {DonationCompleteComponent} from './donation-complete.component';
+import {TBG_DONATE_ID_STORAGE} from '../identity.service';
 import {Donation} from "../donation.model";
-import {CampaignService} from "../campaign.service";
-import {PageMetaService} from "../page-meta.service";
-
-function createComponentWithHandMadeFakes() {
-  const donationService = {
-    // get: () => ({subscribe: () => Promise.resolve(largeDonation)}),
-    // getDonation: () => (largeDonation),
-    // removeOldLocalDonations: () => null,
-    updateLocalDonation: () => null,
-  }
-
-  const route = {
-    params: {
-      pipe: () => ({subscribe: () => Promise.resolve({donationId: 42})})
-    }
-  };
-
-  const identityService = {
-    getIdAndJWT: () => false
-  };
-
-  const campaignService = {
-    getOneById: () => ({subscribe: () => null})
-  };
-
-  const analyticsService = {
-    logEvent: () => null,
-  };
-
-  const component = new DonationCompleteComponent(
-    analyticsService as unknown as AnalyticsService,
-    campaignService as unknown as CampaignService,
-    null as unknown as MatDialog,
-    donationService as unknown as DonationService,
-    identityService as unknown as IdentityService,
-    null as unknown as PageMetaService,
-    route as unknown as ActivatedRoute
-  );
-  return component;
-}
 
 describe('DonationCompleteComponent', () => {
   let analyticsService: AnalyticsService;
@@ -124,14 +84,32 @@ describe('DonationCompleteComponent', () => {
   }
 
   it('Considers donation of £5k to be large', () => {
-    const largeDonation = donationOf(5_000, "GBP");
-  //  const component = createComponentWithHandMadeFakes();
-
     // Calling private setDonation method for test. Not a fan of doing this, but I couldn't work out how to get the
     // async stuff to happen to make this happen via a call to checkDonation in the test.
     // @ts-ignore
-    component.setDonation(largeDonation);
+    component.setDonation(donationOf(5_000, "GBP"));
 
     expect(component.donationIsLarge).toBeTrue();
+  });
+
+  it('Considers donation of under £5k to be not large', () => {
+    // @ts-ignore
+    component.setDonation(donationOf(4999, "GBP"));
+
+    expect(component.donationIsLarge).toBeFalse();
+  });
+
+  it('Considers donation of €5k to be not large', () => {
+    // @ts-ignore
+    component.setDonation(donationOf(5000, "EUR"));
+
+    expect(component.donationIsLarge).toBeFalse();
+  });
+
+  it('Considers donation of under €5k to be not large', () => {
+    // @ts-ignore
+    component.setDonation(donationOf(4999, "EUR"));
+
+    expect(component.donationIsLarge).toBeFalse();
   });
 });
