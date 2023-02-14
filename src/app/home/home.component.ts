b1a0ea80f57d9e310685b1debcfeede203944613
Barney Laurance <barney@thebiggive.org.uk>
2023-02-14 16:26:33 +0000

    DON-738: Homepage updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PageMetaService } from '../page-meta.service';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-home',
@@ -9,8 +10,13 @@ import { PageMetaService } from '../page-meta.service';
 })
 export class HomeComponent implements OnInit {
   mainTitle = 'Matching Donations.\nMultiplying Impact.';
-  highlightCard1Title = 'Double your donation\nin the Christmas Challenge 2022';
-  highlightCard1Subtitle = 'Donate between\n29 Nov – 6 Dec';
+
+  viewingTime: Date;
+  showMHFSaveTheDate: boolean;
+  showMNFNowOpen: boolean;
+  showGreenMatchFund: boolean;
+  showChampionsForChildren: boolean;
+  showExplore: boolean;
 
   public constructor(private pageMeta: PageMetaService) {}
 
@@ -20,5 +26,16 @@ export class HomeComponent implements OnInit {
       'Big Give – discover campaigns and donate',
       'https://images-production.thebiggive.org.uk/0011r00002IMRknAAH/CCampaign%20Banner/db3faeb1-d20d-4747-bb80-1ae9286336a3.jpg',
     );
+
+    const viewingTime = new Date();
+
+    this.showMHFSaveTheDate = viewingTime < new Date("2023-02-19T23:59:00.000+00:00");
+    this.showMNFNowOpen = ! this.showMHFSaveTheDate && viewingTime < new Date("2023-03-26T23:59:00.000+00:00");
+
+    this.showGreenMatchFund = viewingTime < new Date("2023-02-19T23:59:00.000+00:00");
+
+    this.showGreenMatchFund = viewingTime < new Date("2023-02-19T23:59:00.000+00:00");
+    this.showChampionsForChildren = viewingTime < new Date("2023-02-26T23:59:00.000+00:00");
+    this.showExplore = viewingTime >= new Date("2023-02-19T23:59:00.000+00:00");
   }
 }
