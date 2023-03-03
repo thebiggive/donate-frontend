dbb2a19c12179adb4fcacdc4368d3fa44c2b7683
Barney Laurance <barney@thebiggive.org.uk>
2023-03-03 15:23:33 +0000

    ID-36: Display error message sent in HTML from server in preference to plain text on password setting failure

--- src/app/donation-complete/donation-complete.component.ts
+++ src/app/donation-complete/donation-complete.component.ts
@@ -18,6 +18,7 @@ import { Person } from '../person.model';
 import { minPasswordLength } from 'src/environments/common';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import {flags} from "../featureFlags";
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-donation-complete',
@@ -40,6 +41,7 @@ export class DonationCompleteComponent implements OnInit {
   recaptchaIdSiteKey = environment.recaptchaIdentitySiteKey;
   registerError?: string;
   registerErrorDescription?: string = undefined;
+  registerErrorDescriptionHtml?: SafeHtml = undefined;
   registrationComplete = false;
   shareUrl: string;
   timedOut = false;
@@ -64,6 +66,7 @@ export class DonationCompleteComponent implements OnInit {
     private identityService: IdentityService,
     private pageMeta: PageMetaService,
     private route: ActivatedRoute,
+    private sanitizer: DomSanitizer,
   ) {
     route.params.pipe().subscribe(params => {
       this.donationId = params.donationId;
@@ -165,8 +168,12 @@ export class DonationCompleteComponent implements OnInit {
           }
         },
         (error: HttpErrorResponse) => {
+          const htmlErrorDescription = error.error?.error?.htmlDescription;
           if (error.error?.error?.type === "DUPLICATE_EMAIL_ADDRESS_WITH_PASSWORD") {
             this.registerErrorDescription = "Your password could not be set. There is already a password set for your email address.";
+          } else if (htmlErrorDescription) {
+            // we bypass security because we trust the Identity server.
+            this.registerErrorDescriptionHtml = this.sanitizer.bypassSecurityTrustHtml(htmlErrorDescription)
           } else {
             this.registerErrorDescription = error.error?.error?.description
           }
