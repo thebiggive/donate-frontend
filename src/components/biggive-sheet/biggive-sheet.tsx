ce2fe874e02b88a585af5535e3348ff16d958361
tomwpaterson-shape <tom@shapehistory.com>
2023-03-07 10:40:15 +0000

    Update to pass build tests

--- src/components/biggive-sheet/biggive-sheet.tsx
+++ src/components/biggive-sheet/biggive-sheet.tsx
@@ -19,14 +19,12 @@ export class BiggiveSheet {
   @Prop() textColour: brandColour = 'white';
 
   openSheet(hash: string) {
-
     if (hash != '' && hash == '#' + this.sheetId) {
       this.host.shadowRoot?.querySelector('.container')?.classList.add('active');
     }
   }
 
   componentWillLoad() {
-
     this.openSheet(window.location.hash);
 
     window.onhashchange = () => {
