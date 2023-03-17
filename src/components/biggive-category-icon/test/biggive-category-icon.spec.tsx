c113a31a17148b59c060ec5ffd5c2d56dc4ad403
Noel Light-Hilary <noel@noellh.com>
2023-03-17 07:07:42 +0000

    Revert "Update libs"
    This reverts commit 75d24032e8ac840bac0cbffc09d6af55d94af87a.

--- src/components/biggive-category-icon/test/biggive-category-icon.spec.tsx
+++ src/components/biggive-category-icon/test/biggive-category-icon.spec.tsx
@@ -13,7 +13,7 @@ describe('biggive-category-icon', () => {
           <div class="background-colour-primary category-icon-item">
             <a href="#">
               <svg class="fill-white" height="512" viewBox="0 0 448 512" width="448" xmlns="http://www.w3.org/2000/svg">
-                <path d="M240 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H176V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H240V80z"></path>
+                <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"></path>    
               </svg>
             </a>
           </div>
