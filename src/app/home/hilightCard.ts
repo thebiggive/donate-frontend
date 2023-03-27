3c1043ee23302ac092aff09bcf70af89b3f2f192
Barney Laurance <barney@thebiggive.org.uk>
2023-03-27 11:30:12 +0100

    DON-735: Refactor - move hard coded homepage hilight content out of template into typescript
    Hopefully this will be useful as a stepping stone to pulling it from Salesforce

---
+++ src/app/home/hilightCard.ts
@@ -0,0 +1,10 @@
+export type highlightCard = {
+  backgroundImageUrl: URL,
+  iconColor: 'primary' | 'brand-4',
+  headerText: string,
+  bodyText: string,
+  button: {
+      text: string,
+      href: URL,
+  },
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 
 import {PageMetaService} from '../page-meta.service';
+import {highlightCard} from "./hilightCard";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-home',
@@ -10,9 +12,34 @@ import {PageMetaService} from '../page-meta.service';
 export class HomeComponent implements OnInit {
   mainTitle = 'Matching Donations.\nMultiplying Impact.';
 
+  highlightCards: readonly highlightCard[];
+
   public constructor(private pageMeta: PageMetaService) {}
 
   ngOnInit() {
+    this.highlightCards = [
+      {
+        headerText: 'Turkey-Syria Earthquake Appeal',
+        bodyText: "Double the impact of your donation",
+        iconColor: "brand-4",
+        backgroundImageUrl: new URL('/assets/images/emergency-card.png', environment.donateUriPrefix),
+        button: {
+          text: "Donate now",
+          href: new URL('/turkey-syria-earthquake-appeal', environment.donateUriPrefix),
+        }
+      },
+      {
+        headerText: 'One donation. Twice the impact.',
+        bodyText: "You donate.\nWe double it.",
+        iconColor: "primary",
+        backgroundImageUrl: new URL('/assets/images/blue-texture.jpg', environment.donateUriPrefix),
+        button: {
+          text: "Explore now",
+          href: new URL('/explore', environment.donateUriPrefix),
+        }
+      },
+    ];
+
     this.pageMeta.setCommon(
       'Big Give',
       'Big Give – discover campaigns and donate',
