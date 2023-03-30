d49e9a3cf0319f80dcaef8b75b4950060287f94d
Dorota Hawro <dorota@thebiggive.org.uk>
2023-03-30 10:02:35 +0100

    Fix linting errors

--- src/components/biggive-footer/biggive-footer.tsx
+++ src/components/biggive-footer/biggive-footer.tsx
@@ -10,7 +10,7 @@ export class BiggiveFooter {
 
   @Prop() headingLevel: 1 | 2 | 3 | 4 | 5 | 6 = 5;
 
-    /**
+  /**
    * Conditionally render footer menu:
    * hard-coded (preset) when set to true, dynamic (slot-based) when set to false
    */
@@ -35,48 +35,48 @@ export class BiggiveFooter {
   render() {
     const HeadingTag = `h${this.headingLevel}`;
     const slotBasedFooter = () => {
-    return (
-      <footer class="footer">
-        <div class="row row-top">
-          <nav class="nav nav-primary" aria-labelledby="footer-primary-heading">
-            <HeadingTag class="heading" id="footer-primary-heading">
-              <slot name="nav-primary-title"></slot>
-            </HeadingTag>
-          </nav>
+      return (
+        <footer class="footer">
+          <div class="row row-top">
+            <nav class="nav nav-primary" aria-labelledby="footer-primary-heading">
+              <HeadingTag class="heading" id="footer-primary-heading">
+                <slot name="nav-primary-title"></slot>
+              </HeadingTag>
+            </nav>
 
-          <nav class="nav nav-secondary" aria-labelledby="footer-secondary-heading-heading">
-            <HeadingTag class="heading" id="footer-secondary-heading">
-              <slot name="nav-secondary-title"></slot>
-            </HeadingTag>
-          </nav>
+            <nav class="nav nav-secondary" aria-labelledby="footer-secondary-heading-heading">
+              <HeadingTag class="heading" id="footer-secondary-heading">
+                <slot name="nav-secondary-title"></slot>
+              </HeadingTag>
+            </nav>
 
-          <nav class="nav nav-tertiary" aria-labelledby="footer-tertiary-heading">
-            <HeadingTag class="heading" id="footer-tertiary-heading">
-              <slot name="nav-tertiary-title"></slot>
-            </HeadingTag>
-          </nav>
+            <nav class="nav nav-tertiary" aria-labelledby="footer-tertiary-heading">
+              <HeadingTag class="heading" id="footer-tertiary-heading">
+                <slot name="nav-tertiary-title"></slot>
+              </HeadingTag>
+            </nav>
 
-          <div class="button-wrap">
-            <biggive-button colour-scheme="white" url="https://blog.thebiggive.org.uk/charities" label="For charities"></biggive-button>
-            <biggive-button colour-scheme="white" url="https://blog.thebiggive.org.uk/funders" label="For funders"></biggive-button>
-          </div>
-        </div>
-
-        <div class="row row-bottom">
-          <div class="postscript-wrap">
-            <img class="fr-logo" src={getAssetPath('../assets/images/fundraising-regulator.png')} alt="Fundraising Regulator" />
-
-            <nav class="nav nav-postscript" aria-label="Legal"></nav>
+            <div class="button-wrap">
+              <biggive-button colour-scheme="white" url="https://blog.thebiggive.org.uk/charities" label="For charities"></biggive-button>
+              <biggive-button colour-scheme="white" url="https://blog.thebiggive.org.uk/funders" label="For funders"></biggive-button>
+            </div>
           </div>
 
-          <div class="social-icon-wrap">
-            <slot name="social-icons"></slot>
+          <div class="row row-bottom">
+            <div class="postscript-wrap">
+              <img class="fr-logo" src={getAssetPath('../assets/images/fundraising-regulator.png')} alt="Fundraising Regulator" />
+
+              <nav class="nav nav-postscript" aria-label="Legal"></nav>
+            </div>
+
+            <div class="social-icon-wrap">
+              <slot name="social-icons"></slot>
+            </div>
+            <p>&copy; 2007 – 2022 The Big Give Trust (1136547) | Company number 07273065 | Dragon Court, 27-29 Macklin Street, London WC2B 5LX, United Kingdom</p>
           </div>
-          <p>&copy; 2007 – 2022 The Big Give Trust (1136547) | Company number 07273065 | Dragon Court, 27-29 Macklin Street, London WC2B 5LX, United Kingdom</p>
-        </div>
-      </footer>
-    );
-    }
+        </footer>
+      );
+    };
 
     const presetFooter = () => {
       return (
@@ -227,4 +227,3 @@ export class BiggiveFooter {
     return this.usePresetFooter ? presetFooter() : slotBasedFooter();
   }
 }
-
