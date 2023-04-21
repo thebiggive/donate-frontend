6842075cdefa8644aa967c533e7bccddc3c3ec9a
Barney Laurance <barney@thebiggive.org.uk>
2023-04-21 11:43:11 +0100

    DON-704: Create empty Donation Start form component
    I just ran `ng generate component --standalone DonationStartForm`

---
+++ src/app/donation-start-form/donation-start-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-donation-start-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './donation-start-form.component.html',
+  styleUrls: ['./donation-start-form.component.scss']
+})
+export class DonationStartFormComponent {
+
+}
