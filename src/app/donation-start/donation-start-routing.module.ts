99dd394bbd119cebb24d41d9edf3d5862fa1cb50
Barney Laurance <barney@thebiggive.org.uk>
2023-04-21 14:50:00 +0100

    DON-704: Wrap donation start component in container

--- src/app/donation-start/donation-start-routing.module.ts
+++ src/app/donation-start/donation-start-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DonationStartComponent } from './donation-start.component';
+import {DonationStartContainerComponent} from "./donation-start-container/donation-start-container.component";
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: DonationStartComponent,
+    component: DonationStartContainerComponent,
   },
 ];
 
