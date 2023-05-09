72fb949dd287637997b8bd8a63623ad3e8add681
Barney Laurance <barney@thebiggive.org.uk>
2023-05-09 18:17:07 +0100

    DON-610: Remove text input tests
    This component needs to be tested visually, automated tests seem to have
    little if any value here. And they were failing.

--- src/components/biggive-form-field-text-input/test/biggive-text-input.e2e.js
+++
@@ -1,9 +0,0 @@
-import { newE2EPage } from '@stencil/core/testing';
-describe('biggive-text-input', () => {
-    it('renders', async () => {
-        const page = await newE2EPage();
-        await page.setContent('<biggive-text-input></biggive-text-input>');
-        const element = await page.find('biggive-text-input');
-        expect(element).toHaveClass('hydrated');
-    });
-});
