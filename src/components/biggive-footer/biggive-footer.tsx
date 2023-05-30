6aedfda2776e7945d7a0ced30420f2c7a93f4391
Noel Light-Hilary <noel@noellh.com>
2023-05-30 14:27:43 +0100

    Happy new year!

--- src/components/biggive-footer/biggive-footer.tsx
+++ src/components/biggive-footer/biggive-footer.tsx
@@ -80,7 +80,7 @@ export class BiggiveFooter {
             <div class="social-icon-wrap">
               <slot name="social-icons"></slot>
             </div>
-            <p>&copy; 2007 – 2022 The Big Give Trust (1136547) | Company number 07273065 | Dragon Court, 27-29 Macklin Street, London WC2B 5LX, United Kingdom</p>
+            <p>&copy; 2007 – 2023 The Big Give Trust (1136547) | Company number 07273065 | Dragon Court, 27-29 Macklin Street, London WC2B 5LX, United Kingdom</p>
           </div>
         </footer>
       );
@@ -226,7 +226,7 @@ export class BiggiveFooter {
                 <biggive-social-icon service="Instagram" url="https://www.instagram.com/biggiveorg" background-colour="tertiary" icon-colour="black"></biggive-social-icon>
               </div>
             </div>
-            <p>&copy; 2007 – 2022 The Big Give Trust (1136547) | Company number 07273065 | Dragon Court, 27-29 Macklin Street, London WC2B 5LX, United Kingdom</p>
+            <p>&copy; 2007 – 2023 The Big Give Trust (1136547) | Company number 07273065 | Dragon Court, 27-29 Macklin Street, London WC2B 5LX, United Kingdom</p>
           </div>
         </footer>
       );
