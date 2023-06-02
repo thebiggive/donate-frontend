1746b2e31393411305256d684d77594dddd09de7
Barney Laurance <barney@thebiggive.org.uk>
2023-06-02 16:08:26 +0100

    DON-690: Refactor, pull all members of DonationStartFormNewComponet up to parent
    This will make it easy to remove the class later.

--- src/app/donation-start/donation-start-form/donation-start-form-new.component.ts
+++ src/app/donation-start/donation-start-form/donation-start-form-new.component.ts
@@ -2,10 +2,9 @@
  * TODO: should move most of the declarations up to the parent component!!!
  */
 import {CurrencyPipe} from "@angular/common";
-import {Component, ViewChild} from "@angular/core";
+import {Component} from "@angular/core";
 import {TimeLeftPipe} from "src/app/time-left.pipe";
 import {DonationStartFormParentComponent} from "./donation-start-form-parent.component";
-import {DonationTippingSliderComponent} from "./donation-tipping-slider/donation-tipping-slider.component";
 
 @Component({
   selector: 'app-donation-start-form-new',
@@ -17,22 +16,5 @@ import {DonationTippingSliderComponent} from "./donation-tipping-slider/donation
   ]
 })
 export class DonationStartFormNewComponent extends DonationStartFormParentComponent {
-
-  panelOpenState = false;
-  percentage = 1;
-  showCustomTipInput = false;
-  @ViewChild('donationTippingSlider') tippingSlider: DonationTippingSliderComponent;
-
-  displayCustomTipInput = () => {
-    if (this.tipValue) {
-      this.amountsGroup.get('tipAmount')?.setValue(this.tipValue.toString());
-    }
-    this.tipValue = undefined;
-    this.showCustomTipInput = true;
-  }
-
-  displayPercentageTipInput = () => {
-    this.tippingSlider.setTipAmount(this.tipAmount());
-    this.showCustomTipInput = false;
-  }
+  // this class intentionally left empty - it only exists as a place to hang a @Component decorator. Everything else is in parent class.
 }
