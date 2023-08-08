ba1aabc64b5e1b8c23c1614c3b3e6c80729db400
Barney Laurance <barney@thebiggive.org.uk>
2023-08-08 18:01:04 +0100

    DON-783: WIP cookie consent banner
    Doesn't do anything yet, will either disable or make appear only in
    dev/staging before I merge tomorrow.

--- src/app/cookie-consent-banner/cookie-consent-banner.component.ts
+++ src/app/cookie-consent-banner/cookie-consent-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-cookie-consent-banner',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class CookieConsentBannerComponent {
 
+  protected readonly environment = environment;
 }
