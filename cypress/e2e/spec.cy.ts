92591ba8e21dbac332445fe932e79f8651015bd8
Noel Light-Hilary <noel@biggive.org>
2023-09-19 18:05:19 +0100

    Update cypress/e2e/spec.cy.ts
    Make verb reference the app itself
    
    Co-authored-by: Barney Laurance <barney@thebiggive.org.uk>

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
 describe('App boot fundamentals', () => {
-  it('Sees our headline copy on the Donate home page', () => {
+  it('Shows our headline copy on the Donate home page', () => {
     cy.visit('/')
     cy.get('biggive-hero-image').shadow().contains('Matching Donations.')
   })
