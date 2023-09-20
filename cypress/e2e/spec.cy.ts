04954e5144837b2e6770c07f498e8b0e2a7c89ea
Barney Laurance <barney@thebiggive.org.uk>
2023-09-20 13:39:02 +0100

    DON-614: Intercept HTTP call to SF API in cypress tests
    Makes the frontend a little more self-contained, although we lose some
    incidental testing of the SF API

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,20 @@
+import {CampaignStats} from "../../src/app/campaign-stats.model";
+
 describe('App boot fundamentals', () => {
-  it('Shows our headline copy on the Donate home page', () => {
-    cy.visit('/')
-    cy.contains('Matching Donations.')
+  beforeEach(() => {
+    cy.intercept(
+      {url: "https://sf-api-staging.thebiggivetest.org.uk//campaigns/services/apexrest/v1.0/campaigns/stats"},
+      {
+        body: {totalRaised: 500_000, totalCampaignCount: 123_456} as CampaignStats
+      }
+    )
+  })
+
+  it('Shows our copy on the Donate home page', () => {
+    cy.visit('/');
+
+    cy.contains('Matching Donations.');
+    cy.contains('£500,000');
+    cy.contains('raised for over 123,456 charity projects since 2008');
   })
 })
