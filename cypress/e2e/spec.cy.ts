64b534aeb1b0a7d7729b7a53e39e3b04f74d86d1
Barney Laurance <barney@thebiggive.org.uk>
2023-09-20 10:28:28 +0100

    Add failing cyprus test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,4 +3,10 @@ describe('App boot fundamentals', () => {
     cy.visit('/')
     cy.contains('Matching Donations.')
   })
+
+  it('Mentions exploration on the explore page', () => {
+    // test we know will fail, just to check how cyprus failures look and to see that we can see the screenshot as a circle artefact.
+    cy.visit('/explore')
+    cy.contains('exploration')
+  })
 })
