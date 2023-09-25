c1962b99dd9cef01991ab7f039e0b1159d5fc203
Dorota Hawro <dorota@thebiggive.org.uk>
2023-09-25 16:45:41 +0100

    DON-874: use brandColor type

--- src/app/home/HighlightCard.ts
+++ src/app/home/HighlightCard.ts
@@ -6,29 +6,7 @@ export type HighlightCard = {
   // use brand-mhf-turquoise instead.
   // See https://github.com/thebiggive/donate-frontend/pull/1076#issuecomment-1523472452
   // For other colour details see https://github.com/thebiggive/components/blob/develop/src/globals/brand-colour.ts
-  iconColor: 'primary' |
-  'tertiary' |
-  'brand-1' |
-  'brand-2' |
-  'brand-3' |
-  'brand-4' |
-  'brand-5' |
-  'brand-6' |
-  'brand-mhf-turquoise' |
-  'brand-wgmf-purple' |
-  'brand-cc-red' |
-  'brand-wgmf-purple' |
-  'brand-gmf-green' |
-  'brand-emf-yellow' |
-  'brand-c4c-orange' |
-  'brand-afa-pink' |
-  'brand-grey' |
-  'white' |
-  'black' |
-  'grey-extra-light' |
-  'grey-light' |
-  'grey-medium' |
-  'grey-dark',
+  iconColor: brandColour,
   headerText: string,
   bodyText: string,
   button: {
