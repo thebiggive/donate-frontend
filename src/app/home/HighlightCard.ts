4b788eae34ed20c06a66257afd3fb356f634f78d
Dorota Joanna Hawro <57061244+dorota-joanna@users.noreply.github.com>
2023-09-25 17:15:46 +0100

    Merge pull request #1307 from thebiggive/DON-874-update-homepage-highlight-cards
    DON-874: Update Homepage Highlight Cards:

--- src/app/home/HighlightCard.ts
+++ src/app/home/HighlightCard.ts
@@ -1,10 +1,12 @@
+import { brandColour } from "@biggive/components/dist/types/globals/brand-colour"
+
 export type HighlightCard = {
   backgroundImageUrl: URL,
   // There is ambiguity in the components library about whether brand-6 is grey or turquoise. Best to avoid using brand-6 and
   // use brand-mhf-turquoise instead.
   // See https://github.com/thebiggive/donate-frontend/pull/1076#issuecomment-1523472452
   // For other colour details see https://github.com/thebiggive/components/blob/develop/src/globals/brand-colour.ts
-  iconColor: 'primary' | 'tertiary' | 'brand-1' | 'brand-2' | 'brand-3' | 'brand-4' | 'brand-5' | 'brand-6' | 'brand-mhf-turquoise',
+  iconColor: brandColour,
   headerText: string,
   bodyText: string,
   button: {
