509547bb93fb31f2312ac9c9036523851bf3c843
Barney Laurance <barney@thebiggive.org.uk>
2023-10-05 10:23:55 +0100

    DON-783: Enable cookie consent banner in all environments

--- src/app/featureFlags.ts
+++ src/app/featureFlags.ts
@@ -3,9 +3,6 @@ import {EnvironmentID} from "../environments/environment.interface";
 
 export const flagsForEnvironment = (_: EnvironmentID) => {
   return {
-    // Banner needs more work to look right and be useful before release - see ticket
-    // DON-783
-    cookieBannerEnabled: (environment.environmentId === 'development' || environment.environmentId === 'staging'),
     don819FlagEnabled: (environment.environmentId === 'development'),
   };
 }
