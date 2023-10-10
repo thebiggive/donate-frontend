256dfed4d370680d4d9358a13f9737af364500e8
Barney Laurance <barney@thebiggive.org.uk>
2023-10-10 12:39:55 +0100

    DON-889: Update WGMF home page card at noon tomorrow for opening

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,27 +4,28 @@ import {PageMetaService} from '../page-meta.service';
 import {HighlightCard} from "./HighlightCard";
 import {environment} from "../../environments/environment";
 
-const ArtsForImpactCard: HighlightCard = {
-  headerText: "Applications for Arts for Impact are now open!",
-  backgroundImageUrl: new URL('/assets/images/red-coral-texture.png', environment.donateGlobalUriPrefix),
-  iconColor: 'brand-afa-pink',
-  bodyText: 'Apply by 15th December 2023',
-  button: {
-    text: "Apply now",
-    href: new URL('/artsforimpact/', environment.blogUriPrefix)
-  }
-} as const;
+const wgmfCampaignIsOpen = new Date() > new Date('2023-10-11T12:00:00');
 
-const SignUpCard: HighlightCard = {
-  headerText: 'Join our mailing list!',
-  backgroundImageUrl: new URL('/assets/images/anchor-match-fund.jpg', environment.donateGlobalUriPrefix),
-  iconColor: 'primary',
-  bodyText: 'Get the latest updates and campaign reminders straight to your inbox',
+const WGMFCard = wgmfCampaignIsOpen ? {
+  headerText: 'Double your donation in the Women and Girls Match Fund',
+  bodyText: "Donate between 11th - 18th October 2023",
+  iconColor: "brand-wgmf-purple",
+  backgroundImageUrl: new URL('/assets/images/wmg-purple-texture.jpg', environment.donateGlobalUriPrefix),
   button: {
-    text: 'Sign up now',
-    href: new URL('http://eepurl.com/bDbW3r')
+    text: "Donate Now",
+    href: new URL('/women-and-girls-2023/', environment.donateGlobalUriPrefix),
   }
-} as const;
+} as const :
+  {
+    headerText: 'Save the date for Women and Girls Match Fund',
+    bodyText: "11th - 18th October 2023",
+    iconColor: "brand-wgmf-purple",
+    backgroundImageUrl: new URL('/assets/images/wmg-purple-texture.jpg', environment.donateGlobalUriPrefix),
+    button: {
+      text: "Find out more",
+      href: new URL('/women-and-girls-2023/', environment.donateGlobalUriPrefix),
+    }
+  } as const;
 
 @Component({
   selector: 'app-home',
@@ -40,16 +41,7 @@ export class HomeComponent implements OnInit {
 
 
   highlightCards: readonly HighlightCard[] = [
-    {
-      headerText: 'Save the date for Women and Girls Match Fund',
-      bodyText: "11th - 18th October 2023",
-      iconColor: "brand-wgmf-purple",
-      backgroundImageUrl: new URL('/assets/images/wmg-purple-texture.jpg', environment.donateGlobalUriPrefix),
-      button: {
-        text: "Find out more",
-        href: new URL('/women-and-girls-2023/', environment.donateGlobalUriPrefix),
-      }
-    },
+    WGMFCard,
     {
       headerText: 'Double your donation for Global’s Make Some Noise',
       backgroundImageUrl: new URL('/assets/images/blue-texture.jpg', environment.donateGlobalUriPrefix),
@@ -60,7 +52,16 @@ export class HomeComponent implements OnInit {
         href: new URL('/campaign/a056900001xpxqVAAQ', environment.donateGlobalUriPrefix)
       }
     },
-    new Date() > new Date('2023-10-09T12:00:00') ? ArtsForImpactCard : SignUpCard,
+    {
+      headerText: "Applications for Arts for Impact are now open!",
+      backgroundImageUrl: new URL('/assets/images/red-coral-texture.png', environment.donateGlobalUriPrefix),
+      iconColor: 'brand-afa-pink',
+      bodyText: 'Apply by 15th December 2023',
+      button: {
+        text: "Apply now",
+        href: new URL('/artsforimpact/', environment.blogUriPrefix)
+      }
+    } as const,
   ];
 
   public constructor(
