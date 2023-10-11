78af3158e0bffecb25f276afc437253eb00c4a46
Barney Laurance <barney@thebiggive.org.uk>
2023-10-11 10:28:14 +0100

    DON-889: Remove "skip to content" link in prod to avoid releasing before QA

--- src/app/featureFlags.ts
+++ src/app/featureFlags.ts
@@ -4,6 +4,7 @@ import {EnvironmentID} from "../environments/environment.interface";
 export const flagsForEnvironment = (environmentId: EnvironmentID) => {
   return {
     don819FlagEnabled: (environmentId === 'development' || environmentId == 'staging'),
+    skipToContentLinkEnabled: environmentId !== 'production',
   };
 }
 
