cd1372514f8068e6d9ba40e28f04c568ba2ba007
Barney Laurance <barney@thebiggive.org.uk>
2023-10-16 16:41:20 +0100

    MAT-306: Create donor account records in matchbot

---
+++ src/app/donor-account.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../environments/environment";
+import {Person} from "./person.model";
+import {IdentityService} from "./identity.service";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DonorAccountService {
+  constructor(
+    private http: HttpClient,
+    private identityService: IdentityService,
+  ) {
+  }
+
+  createAccount(donor: Person): Observable<unknown> {
+    const jwt = this.identityService.getJWT();
+    if (!jwt) {
+      throw new Error("Missing JWT, can't create donor account");
+    }
+
+    return this.http.post(
+      `${environment.donationsApiPrefix}/${donor.id}/donor-account`,
+      {emailAddress: donor.email_address},
+      {headers: new HttpHeaders({'X-Tbg-Auth': jwt})}
+    );
+  }
+}
