1b55c4e95792e7f94a1bcc06fc05c0fb51ceebe4
Noel Light-Hilary <noel@noellh.com>
2023-10-24 06:33:04 +0100

    DON-472 – update the /charity/... campaign list
    * Use the new design; retire the old campaign-card
    * Sort with newest (last ending) campaign first

--- src/app/charity/charity.component.ts
+++ src/app/charity/charity.component.ts
@@ -1,8 +1,10 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { CampaignSummary } from '../campaign-summary.model';
 import { PageMetaService } from '../page-meta.service';
+import { CampaignService } from '../campaign.service';
 
 @Component({
   selector: 'app-charity',
@@ -13,17 +15,37 @@ export class CharityComponent implements OnInit {
   campaigns: CampaignSummary[];
 
   constructor(
+    private datePipe: DatePipe,
     private pageMeta: PageMetaService,
     private route: ActivatedRoute,
   ) {}
 
   ngOnInit(): void {
-    this.campaigns = this.route.snapshot.data.campaigns;
+    this.campaigns = this.route.snapshot.data.campaigns.sort((a: CampaignSummary, b: CampaignSummary) => {
+      return new Date(b.endDate).getTime() - new Date(a.endDate).getTime();
+    });
 
     this.pageMeta.setCommon(
       this.campaigns[0] ? `${this.campaigns[0].charity.name} Campaigns` : 'Campaigns Archive',
       'Archive of Big Give match funded campaigns',
       null,
     );
+  }
+
+  getPercentageRaised(childCampaign: CampaignSummary) {
+    CampaignService.percentRaisedOfIndividualCampaign(childCampaign);
+  }
+
+  getRelevantDateAsStr(campaign: CampaignSummary) {
+    const date = CampaignService.getRelevantDate(campaign);
+    return date ? this.datePipe.transform(date, 'dd/MM/yyyy, HH:mm') : null;
+  }
+
+  isInFuture(campaign: CampaignSummary) {
+    return CampaignService.isInFuture(campaign);
+  }
+
+  isInPast(campaign: CampaignSummary) {
+    return CampaignService.isInPast(campaign);
   }
 }
