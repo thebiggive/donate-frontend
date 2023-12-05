4542c25499626e1527c684073c9a3377516eccac
Barney Laurance <barney@thebiggive.org.uk>
2023-12-05 17:35:14 +0000

    DON-752: Add spinner to show when login request processing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -74,7 +74,7 @@ export class LoginComponent implements OnInit{
   }
 
   login(): void {
-    this.loggingIn = true; // todo - add visible indication of this to page, e.g. spinner and removal of button
+    this.loggingIn = true;
     this.captcha.reset();
     this.captcha.execute();
   }
