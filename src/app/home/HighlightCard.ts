bbd6e6530c766cc3f4d6fa075d6ffc34e782b2b8
Barney Laurance <barney@thebiggive.org.uk>
2024-01-26 11:35:48 +0000

    DON-735: Set background images, colours and fix link domains for homepage highlight cards from SF API

--- src/app/home/HighlightCard.ts
+++ src/app/home/HighlightCard.ts
@@ -25,3 +25,59 @@ export type HighlightCard = {
       href: URL,
   },
 }
+
+export type campaignFamilyName = 'christmasChallenge'|'summerGive'|'greenMatchFund'|'womenGirls'|'mentalHealthFund'|'artsforImpact';
+
+
+export type SfApiHighlightCard = Omit<HighlightCard, 'backgroundImageUrl'|'iconColor'|'button'> & {
+  campaignFamily: campaignFamilyName
+  button: {
+    text: string,
+    href: string
+  }
+};
+
+const replaceURLOrigin = (experienceUriPrefix: string, blogUriPrefix: string, donateGlobalUriPrefix: string, urlFromApi: string): URL => {
+
+  const href = urlFromApi
+    .replace('https://donate.biggive.org', donateGlobalUriPrefix)
+    .replace('https://biggive.org', blogUriPrefix)
+    .replace('https://community.biggive.org', experienceUriPrefix)
+  ;
+
+  return new URL(href);
+}
+
+export const SFAPIHighlightCardToHighlightCard = (experienceUriPrefix: string, blogUriPrefix: string, donateGlobalUriPrefix: string, sfApiHighlightCard: SfApiHighlightCard, ): HighlightCard => {
+
+  const campaignFamilyColours: Record<campaignFamilyName, brandColour> = {
+    christmasChallenge: 'brand-cc-red',
+    summerGive: 'brand-c4c-orange',
+    greenMatchFund: 'brand-gmf-green',
+    womenGirls: 'brand-wgmf-purple',
+    mentalHealthFund: 'brand-mhf-turquoise',
+    artsforImpact: 'brand-afa-pink',
+  };
+
+  const campaignFamilyBackgroundImages: Record<campaignFamilyName, URL> = {
+    christmasChallenge: new URL('/assets/images/card-background-cc-lights.jpg', donateGlobalUriPrefix),
+    summerGive:  new URL('/assets/images/colour-orange.png', donateGlobalUriPrefix),
+    greenMatchFund:  new URL('/assets/images/card-background-gmf.jpg', donateGlobalUriPrefix),
+    womenGirls: new URL('/assets/images/turquoise-texture.jpg', donateGlobalUriPrefix),
+    mentalHealthFund: new URL('/assets/images/turquoise-texture.jpg', donateGlobalUriPrefix),
+    artsforImpact: new URL('/assets/images/red-coral-texture.png', donateGlobalUriPrefix),
+  };
+
+  return {
+    appearAt: sfApiHighlightCard.appearAt,
+    disappearAt: sfApiHighlightCard.disappearAt,
+    headerText: sfApiHighlightCard.headerText,
+    bodyText: sfApiHighlightCard.bodyText,
+    iconColor: campaignFamilyColours[sfApiHighlightCard.campaignFamily] || "primary",
+    backgroundImageUrl: campaignFamilyBackgroundImages[sfApiHighlightCard.campaignFamily] || new URL('/assets/images/blue-texture.jpg', donateGlobalUriPrefix),
+    button: {
+      text: sfApiHighlightCard.button.text,
+      href: replaceURLOrigin(experienceUriPrefix, blogUriPrefix, donateGlobalUriPrefix, sfApiHighlightCard.button.href),
+    }
+  };
+};
