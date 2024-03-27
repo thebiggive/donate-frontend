f45453e12871fd4af122716c63f5738716d11a1e
Barney Laurance <barney@thebiggive.org.uk>
2024-03-27 16:19:20 +0000

    DON-950: Fix broken Matomo config
    Previously it was sending a request to
    https://biggive.matomo.cloud/matomo.js/matomo.js which returns a 500.
    With this change it requests https://biggive.matomo.cloud/matomo.js
    which works.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,7 @@ const matomoBaseUri = 'https://biggive.matomo.cloud';
     HttpClientModule,
     NgxMatomoModule.forRoot({
       siteId: environment.matomoSiteId,
-      trackerUrl: `${matomoBaseUri}/matomo.js`,
+      trackerUrl: matomoBaseUri,
       mode: MatomoInitializationMode.AUTO,
       requireConsent: MatomoConsentMode.COOKIE,
     }),
