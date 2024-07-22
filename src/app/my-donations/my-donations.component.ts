260397e337999c248a7104466040304333bcded9
Barney Laurance <barney@thebiggive.org.uk>
2024-07-22 14:34:58 +0100

    DON-983: Create "my donations" component
    Purely the result of running
    ng generate component --standalone my-donations

---
+++ src/app/my-donations/my-donations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-donations',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-donations.component.html',
+  styleUrl: './my-donations.component.css'
+})
+export class MyDonationsComponent {
+
+}
