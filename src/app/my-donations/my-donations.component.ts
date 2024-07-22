324ba193fcc8a6550fe03ebff0e717a8026953bf
Barney Laurance <barney@thebiggive.org.uk>
2024-07-22 14:57:47 +0100

    DON-983: Style donations list page and fill in temporarily with dummy donations

--- src/app/my-donations/my-donations.component.ts
+++ src/app/my-donations/my-donations.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {ComponentsModule} from "@biggive/components-angular";
 
 @Component({
   selector: 'app-my-donations',
   standalone: true,
-  imports: [],
+  imports: [
+    ComponentsModule
+  ],
   templateUrl: './my-donations.component.html',
-  styleUrl: './my-donations.component.css'
+  styleUrl: './my-donations.component.scss'
 })
 export class MyDonationsComponent {
 
