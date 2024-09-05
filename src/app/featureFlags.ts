3f6419b8f0c58afaafdef29a710cbe32f6434900
Barney Laurance <barney@thebiggive.org.uk>
2024-09-05 17:20:44 +0100

    DON-896: Use new stripe element card choice on staging

--- src/app/featureFlags.ts
+++ src/app/featureFlags.ts
@@ -16,7 +16,7 @@ const flagsForEnvironment = (environmentId: EnvironmentID) => {
      * Replaces our own custom display of saved cards that are fetch from Identity, with saved cards
      * inside the stripe payment element on the donation page.
      */
-    stripeElementCardChoice: environmentId === 'development',
+    stripeElementCardChoice: environmentId === 'development' || environmentId === 'staging',
   } as const;
 }
 
