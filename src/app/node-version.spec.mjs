d062c5d2c006bb98343c7fd99a73e05400aec0b4
Barney Laurance <barney@thebiggive.org.uk>
2024-09-27 12:35:31 +0100

    DON-1073: Fix node version checks

--- src/app/node-version.spec.mjs
+++ src/app/node-version.spec.mjs
@@ -9,21 +9,24 @@ import assert from "node:assert/strict";
 import { fileURLToPath } from "node:url";
 import { dirname, resolve } from 'path'
 import {readFileSync} from "node:fs";
+const modulePath = dirname(fileURLToPath(import.meta.url));
+
 const expectedNodeVersion = '20.13.1';
 
-const modulePath = dirname(fileURLToPath(import.meta.url));
+function assertAllNodeMentionsAreExpectedVersion(circleConfig) {
+  const nodeMentions = [...circleConfig.matchAll(/node:[0-9\\.]+/g)];
+  assert(nodeMentions.length > 0);
+  nodeMentions.forEach(match => {
+    assert.equal(match[0], `node:${expectedNodeVersion}`);
+  })
+}
 
 test('should only have the expected node version mentioned in circle config for S3 build', async () => {
   const circleConfig = readFileSync(resolve(modulePath, '../../.circleci/config.yml')).toString();
-  const nodeMentions = [...circleConfig.matchAll(/node[0-9\\.]+/g)];
-  nodeMentions.forEach(match => {
-    assert.equal(match[0], `node${expectedNodeVersion}`);
-  })
+  assertAllNodeMentionsAreExpectedVersion(circleConfig);
 });
 
 test('should have the expected node version mentioned in Dockerfile for ECR/ECS build', async () => {
   const dockerFile = readFileSync(resolve(modulePath, '../../Dockerfile')).toString();
-  const nodeMentions = [...dockerFile.matchAll(/node:[0-9\\.]+/g)];
-  nodeMentions.forEach(match => {
-    assert.equal(match[0], `node:${expectedNodeVersion}`);
-  })})
+  assertAllNodeMentionsAreExpectedVersion(dockerFile);
+});
