a4ed80210e648798e58443c3e1ae9fee0cefce9d
Barney Laurance <barney@thebiggive.org.uk>
2024-10-07 16:35:25 +0100

    DON-998: Display campaign details in sidebar on regular giving page like on one-off donation page

--- src/app/regular-giving/regular-giving.component.ts
+++ src/app/regular-giving/regular-giving.component.ts
@@ -2,23 +2,32 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Campaign} from "../campaign.model";
 import {ComponentsModule} from "@biggive/components-angular";
+import {CampaignInfoComponent} from "../campaign-info/campaign-info.component";
+import {ImageService} from "../image.service";
+import {Observable} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-regular-giving',
   standalone: true,
   imports: [
-    ComponentsModule
+    ComponentsModule,
+    CampaignInfoComponent,
+    AsyncPipe
   ],
   templateUrl: './regular-giving.component.html',
-  styleUrl: './regular-giving.component.css'
+  styleUrl: './regular-giving.component.scss'
 })
 export class RegularGivingComponent implements OnInit{
   protected campaign: Campaign;
+  protected bannerUri$: Observable<string|null>;
   constructor(
     private route: ActivatedRoute,
+    private imageService: ImageService,
   ) {
   }
   ngOnInit() {
     this.campaign = this.route.snapshot.data.campaign;
+    this.bannerUri$ = this.imageService.getImageUri(this.campaign.bannerUri, 830);
   }
 }
