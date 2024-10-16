65bc541b31d223be9cf46d43f036872ae3949297
Barney Laurance <barney@thebiggive.org.uk>
2024-10-16 18:31:52 +0100

    Stop warning about matomo.js in `npm run ci` output

--- karma-local.conf.js
+++ karma-local.conf.js
@@ -30,6 +30,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    proxies: {'/matomo.js': ''}
   });
 };
