52a3d79cab790cb92e2efe3d0f801a48c979b8e9
Noel Light-Hilary <noel@noellh.com>
2024-10-22 15:54:07 +0100

    OPS-770 – set config for new tip A/B test

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -18,10 +18,10 @@ export const environment: Environment = {
   matomoSiteId: 2,
   matomoNonZeroTipGoalId: 11,
   matomoAbTest: {
-    name: 'zeroTipTextAB_wgmf_2024',
+    name: 'zeroTipText_winter_2024',
     variantName: 'B',
-    startDate: '2024/10/10 11:00:00 UTC',
-    endDate: '2024/10/17 11:00:00 UTC',
+    startDate: '2024/10/22 16:00:00 UTC',
+    endDate: '2024/12/10 12:30:00 UTC',
   },
   minimumCreditAmount: 500,
   maximumCreditAmount: 500_000,
