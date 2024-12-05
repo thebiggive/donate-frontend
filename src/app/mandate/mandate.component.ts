93da2f6a7ef4fff6be04718a1c7a091579a98696
Barney Laurance <barney@thebiggive.org.uk>
2024-12-05 17:06:24 +0000

    DON-998: Start mandate page content by copying html from donation thanks page

--- src/app/mandate/mandate.component.ts
+++ src/app/mandate/mandate.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {ComponentsModule} from "@biggive/components-angular";
+import {DatePipe} from "@angular/common";
+import {ExactCurrencyPipe} from "../exact-currency.pipe";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {Campaign} from "../campaign.model";
 
 @Component({
   selector: 'app-mandate',
   standalone: true,
-  imports: [],
+  imports: [
+    ComponentsModule,
+    DatePipe,
+    ExactCurrencyPipe,
+    FaIconComponent,
+    MatProgressSpinner
+  ],
   templateUrl: './mandate.component.html',
   styleUrl: './mandate.component.css'
 })
 export class MandateComponent {
+  complete = true;
+  encodedShareUrl = '';
+  encodedPrefilledText: string = '';
+  donation: any = {};
+  totalValue: 0;
+  campaign: any = {}
+  giftAidAmount: number = 0;
+  totalPaid: 0;
 
 }
