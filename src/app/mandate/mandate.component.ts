04a57c07db63c3cb817e5f8392ff6424208c1ed6
Barney Laurance <barney@thebiggive.org.uk>
2024-12-05 16:37:06 +0000

    Generate blank mandate component (`ng generate component --standalone`)

---
+++ src/app/mandate/mandate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mandate',
+  standalone: true,
+  imports: [],
+  templateUrl: './mandate.component.html',
+  styleUrl: './mandate.component.css'
+})
+export class MandateComponent {
+
+}
