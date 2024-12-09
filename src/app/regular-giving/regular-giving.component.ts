af2699dc6b7d784e84fca6964148b40b71b30260
Dorota Hawro <dorota@thebiggive.org.uk>
2024-12-09 13:04:54 +0000

    DON-998: bring back logging error in browser for non-regular-giving campaigns requested on regular-giving template

--- src/app/regular-giving/regular-giving.component.ts
+++ src/app/regular-giving/regular-giving.component.ts
@@ -64,7 +64,7 @@ export class RegularGivingComponent implements OnInit {
     this.campaign = this.route.snapshot.data.campaign;
 
     if ( !this.campaign.isRegularGiving ) {
-     // throw new Error("Campaign " + this.campaign.id + " is not a regular giving campaign");
+     throw new Error("Campaign " + this.campaign.id + " is not a regular giving campaign");
     }
 
     this.mandateForm = this.formBuilder.group({
