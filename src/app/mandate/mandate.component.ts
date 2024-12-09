9f8111a82e4cfeef2e821ca90935e96e66ddcdb6
Dorota Joanna Hawro <57061244+dorota-joanna@users.noreply.github.com>
2024-12-09 16:53:13 +0000

    Update src/app/mandate/mandate.component.ts
    DON-998: add todo on Mandate object to be fetched from matchbot
    
    Co-authored-by: Barney Laurance <barney@thebiggive.org.uk>

--- src/app/mandate/mandate.component.ts
+++ src/app/mandate/mandate.component.ts
@@ -29,6 +29,7 @@ export class MandateComponent {
   //giftAidAmount: number = 0;
   // TODO: we might want to keep the total donated amount as part of the regular giving mandate
   // totalPaid: number = 0;
+  /** @todo-regular-giving : replace this with a mandate fetched from matchbot *//
   mandate: Mandate = {
     id: 'f7037101-b555-4482-afff-43145fac78bb',
     campaignId: 'a056900002TPVz5AAH',
