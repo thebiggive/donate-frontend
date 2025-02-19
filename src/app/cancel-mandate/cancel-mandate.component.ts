1b33f79f080f75d0ae2298bc6f33309f69b0aa3d
Barney Laurance <barney@thebiggive.org.uk>
2025-02-19 14:13:49 +0000

    DON-1022: Generate cancel mandate component with CLI
    $ ng generate component cancel-mandate --skip-tests --style scss
    
    I don't anticipate this component having enough logic to be worth
    testing.

---
+++ src/app/cancel-mandate/cancel-mandate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cancel-mandate',
+  imports: [],
+  templateUrl: './cancel-mandate.component.html',
+  styleUrl: './cancel-mandate.component.scss'
+})
+export class CancelMandateComponent {
+
+}
