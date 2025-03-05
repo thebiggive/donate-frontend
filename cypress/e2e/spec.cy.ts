3eb523352458fa24be031fa78e1acb28bffcdef4
Noel Light-Hilary <noel@noellh.com>
2025-03-05 16:46:04 +0100

    DON-1125 – make e2e test work without mocking browser stats callout, since there isn't one any more

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,20 +1,13 @@
-import {CampaignStats} from "../../src/app/campaign-stats.model";
-
 describe('App boot fundamentals', () => {
   beforeEach(() => {
-    cy.intercept(
-      {url: "https://sf-api-staging.thebiggivetest.org.uk/campaigns/services/apexrest/v1.0/campaigns/stats"},
-      {
-        body: {totalRaised: 500_000, totalCampaignCount: 123_456} as CampaignStats
-      }
-    )
   })
 
+  // Now we hand over real values from the server, we can't mock callout in the same way so don't
+  // assert a match against specific all-time stats.
   it('Shows our copy on the Donate home page', () => {
     cy.visit('/?noredirect');
 
     cy.contains('Hi. We’re Big Give');
-    cy.contains('£500,000');
-    cy.contains('raised for over 123,456 charity projects since 2008');
+    cy.contains('charity projects since 2008');
   })
 })
