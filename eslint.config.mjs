c6c964a6ac095731271d73c84e323ecb68906f9d
Barney Laurance <barney@thebiggive.org.uk>
2025-04-27 14:07:09 +0100

    Linting: Migrate config from json to mjs
    I followed instructions from
    https://eslint.org/docs/latest/use/configure/migration-guide, just
    running
    ``` npx  @eslint/migrate-config .eslintrc.json ```
    
    and then
    
    ``` npm install @eslint/js @eslint/eslintrc -D ```
    
    Using mjs instead of JSON has the big advantage of allowing comments

---
+++ eslint.config.mjs
@@ -0,0 +1,67 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["projects/**/*"],
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@angular-eslint/recommended",
+    "plugin:@angular-eslint/template/process-inline-templates",
+).map(config => ({
+    ...config,
+    files: ["**/*.ts"],
+})), {
+    files: ["**/*.ts"],
+
+    rules: {
+        "@angular-eslint/directive-selector": ["error", {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+        }],
+
+        "@angular-eslint/component-selector": ["error", {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+        }],
+
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@angular-eslint/prefer-standalone": "off",
+        "no-extra-semi": "off",
+        "prefer-const": "off",
+        "@typescript-eslint/ban-types": "off",
+        "no-extra-boolean-cast": "off",
+        "no-prototype-builtins": "off",
+        "no-var": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "no-unexpected-multiline": "off",
+        "no-useless-escape": "off",
+    },
+}, ...compat.extends(
+    "plugin:@angular-eslint/template/recommended",
+    "plugin:@angular-eslint/template/accessibility",
+).map(config => ({
+    ...config,
+    files: ["**/*.html"],
+})), {
+    files: ["**/*.html"],
+
+    rules: {
+        "@angular-eslint/template/click-events-have-key-events": "error",
+        "@angular-eslint/template/interactive-supports-focus": "error",
+        "@angular-eslint/template/alt-text": "off",
+    },
+}];
