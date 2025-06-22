db9767a4c4989d45086cb4e9aabad782522859fc
Noel Light-Hilary <noel@noellh.com>
2025-06-22 14:55:41 +0100

    DON-1147 – fix Karma CI config, mostly by returning to `@angular-devkit/build-angular`
    Not entirely sure why local Karma was fine and CI broke, suggests a mistake that could probably be remedied to avoid directly
    relying on this package again. But also not particularly necessary not to so going with this for now.

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -8,13 +8,13 @@ process.env.CHROME_BIN = puppeteer.executablePath();
 module.exports = function (config) {
   config.set({
     basePath: "",
-    frameworks: ["jasmine", "@angular/build"],
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage-istanbul-reporter"),
-      require("@angular/build/plugins/karma"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
