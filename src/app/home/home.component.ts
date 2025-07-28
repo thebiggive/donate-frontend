de4c8174b454a2c83f497a43bb2451691b30f309
Barney Laurance <barney@thebiggive.org.uk>
2025-07-28 15:28:26 +0100

    DOM-1120: Refactor, remove excess abstraction around big give banner component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,10 +15,9 @@ import {
   BiggiveCallToAction,
   BiggiveHeading,
   BiggiveButton,
-  BiggiveVideoFeature,
+  BiggiveVideoFeature, BiggiveHeadingBanner,
 } from '@biggive/components-angular';
 import { HighlightCardsComponent } from '../highlight-cards/highlight-cards.component';
-import { HeadingBanner } from '../heading-banner/heading-banner.component';
 import { flags } from '../featureFlags';
 
 @Component({
@@ -33,7 +32,7 @@ import { flags } from '../featureFlags';
     BiggiveHeading,
     BiggiveButton,
     BiggiveVideoFeature,
-    HeadingBanner,
+    BiggiveHeadingBanner,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA], // for swiper
 })
