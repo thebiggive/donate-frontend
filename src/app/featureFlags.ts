7142bdddb2c313d133571d80c5305edebd0d4a42
Barney Laurance <barney@thebiggive.org.uk>
2026-01-28 15:38:17 +0000

    DON-1188: Add organisation option to donor registration form

--- src/app/featureFlags.ts
+++ src/app/featureFlags.ts
@@ -1,18 +1,18 @@
 import { environment } from '../environments/environment';
 import { EnvironmentID } from '../environments/environment.interface';
 
-type flags = { enableEditHomeAddress: boolean; enableWithdrawFunds: boolean };
+type flags = { enableEditHomeAddress: boolean; enableWithdrawFunds: boolean; enableOrgAccount: boolean };
 
 const flagsForEnvironment: (environmentId: EnvironmentID) => flags = (environmentId: EnvironmentID) => {
   switch (environmentId) {
     case 'development':
-      return { enableEditHomeAddress: true, enableWithdrawFunds: true };
+      return { enableEditHomeAddress: true, enableWithdrawFunds: true, enableOrgAccount: true };
     case 'regression':
-      return { enableEditHomeAddress: true, enableWithdrawFunds: true };
+      return { enableEditHomeAddress: true, enableWithdrawFunds: true, enableOrgAccount: false };
     case 'staging':
-      return { enableEditHomeAddress: true, enableWithdrawFunds: true };
+      return { enableEditHomeAddress: true, enableWithdrawFunds: true, enableOrgAccount: false };
     case 'production':
-      return { enableEditHomeAddress: true, enableWithdrawFunds: true };
+      return { enableEditHomeAddress: true, enableWithdrawFunds: true, enableOrgAccount: false };
   }
 };
 
