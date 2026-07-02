7d03d24b63fe1f5c5fb9f51b7fa3c6613d9e680c
Barney Laurance <barney@thebiggive.org.uk>
2026-07-02 14:52:43 +0100

    SO-78: Fix new campaign-filter-grid component, move content back to explore component

--- src/app/explore/campaign-card-filter-grid/campaign-card-filter-grid.component.ts
+++ src/app/explore/campaign-card-filter-grid/campaign-card-filter-grid.component.ts
@@ -1,28 +1,23 @@
-import {Component, inject, Input} from '@angular/core';
-import {SearchService} from "../../search.service";
-import {COUNTRY_CODE} from "../../country-code.token";
-import {CampaignService} from "../../campaign.service";
-import {CampaignSummary} from "../../campaign-summary.model";
+import { Component, inject, Input } from '@angular/core';
+import { SearchService } from '../../search.service';
+import { COUNTRY_CODE } from '../../country-code.token';
+import { flags } from '../../featureFlags';
+import { BiggiveCampaignCardFilterGrid } from '@biggive/components-angular';
 
 @Component({
   selector: 'app-campaign-card-filter-grid',
-  imports: [],
+  imports: [BiggiveCampaignCardFilterGrid],
   templateUrl: './campaign-card-filter-grid.component.html',
   styleUrl: './campaign-card-filter-grid.component.scss',
 })
 export class CampaignCardFilterGridComponent {
-  @Input() categoryOptions: "categoryOptions";
-  @Input() beneficiaryOptions: "beneficiaryOptions";
-  @Input() locationOptions: "locationOptions";
-  @Input() searchService: "searchService.selectedSortLabel";
-  @Input() flags: "flags.enableSearchByLocation";
-  @Input() this
-  @Input() location: "location";
-  @Input() fetchingLocation: "fetchingLocation";
-  @Input() onScroll: "onScroll()";
-  @Input() currencyPipeDigitsInfo: any;
-  @Input() isInFuture: "isInFuture(campaign)";
-  @Input() isInPast: "isInPast(campaign)";
-  @Input() getRelevantDateAsStr: "getRelevantDateAsStr(campaign)";
-  @Input() getPercentageRaised: "campaign.isRegularGiving ? null : getPercentageRaised(campaign)";
+  @Input() categoryOptions: string[] = [];
+  @Input() beneficiaryOptions: string[] = [];
+  @Input() locationOptions: string[] = [];
+  searchService = inject(SearchService);
+  flags = flags;
+  @Input() clientCountryCode = inject(COUNTRY_CODE, { optional: true });
+  @Input() location: GeolocationPosition | undefined;
+  @Input() fetchingLocation: boolean = false;
+  @Input() currencyPipeDigitsInfo: string | undefined;
 }
