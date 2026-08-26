4ffb81d4864e19c8625d7008c54f776e3b85c1cf
shaundbs <shaun@biggive.org>
2026-08-26 15:42:17 +0100

    BG2-3359: Trigger production deploy with comment

--- src/server.ts
+++ src/server.ts
@@ -149,7 +149,7 @@ import { SADMDADomainVerificationFile } from './stripe-apple-developer-merchanti
         'https://www.fundraisingregulator.org.uk',
         'https://web-sdk.ryftpay.com', // Ryft: Loading states, 3D Secure.
         'https://tile.openstreetmap.org',
-        'https://cdn-wp.thebiggive.org.uk',
+        'https://cdn-wp.thebiggive.org.uk', // Wordpress content
         ...imageHosts,
       ],
       'script-src': [
