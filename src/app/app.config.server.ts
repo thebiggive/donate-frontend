0f5c17a3b985321e1307d20397440fe67173c746
Noel Light-Hilary <noel@noellh.com>
2026-09-16 20:42:42 +0100

    INFRA-169 Have server calls out pass SSR token to Cloudflare

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,22 @@
-import { mergeApplicationConfig, ApplicationConfig, ErrorHandler } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, ErrorHandler, Provider } from '@angular/core';
 import { provideServerRendering } from '@angular/ssr';
 
 import { appConfig } from './app.config';
+import { SSR_CLOUDFLARE_TOKEN } from './ssr-token';
+
+const token = process.env['DONATE_CLOUDFLARE_SSR_TOKEN'];
+
+if (!token) {
+  throw new Error('DONATE_CLOUDFLARE_SSR_TOKEN is not configured');
+}
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
+    {
+      provide: SSR_CLOUDFLARE_TOKEN,
+      useValue: token,
+    },
     {
       // overriding custom error handler used on browser to toastify errors and send them to Matomo - on server
       // errors get logged to AWS logs
